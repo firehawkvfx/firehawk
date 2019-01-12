0df4db323e3cadc03e1a1609816fc1a1a4fb993d
user <user@workstation.firehawkvfx.com>
2019-01-12 12:31:52 +1000

    enable nat gateway

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -19,7 +19,7 @@ module "vpc" {
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
-  enable_nat_gateway = false
+  enable_nat_gateway = true
   enable_vpn_gateway = true
 
   tags = {
