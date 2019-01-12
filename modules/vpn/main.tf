fb19f15d1d4ec6793299117e1c94bc456b459fb0
user <user@workstation.firehawkvfx.com>
2019-01-12 11:19:43 +1000

    update vpn to become a submodule

--- modules/vpn/main.tf
+++ modules/vpn/main.tf
@@ -44,7 +44,8 @@ variable "openvpn_user" {}
 variable "openvpn_admin_pw" {}
 
 module "openvpn" {
-  source = "github.com/firehawkvfx/tf_aws_openvpn"
+  #source = "github.com/firehawkvfx/tf_aws_openvpn"
+  source = "./tf_aws_openvpn"
   name   = "openVPN"
 
   # VPC Inputs
