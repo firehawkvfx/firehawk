53605ba08fc8f8f7d0ff1615c917dd5c3d9c0a24
Andrew Graham <andrew@firehawkvfx.com>
2019-01-13 14:21:47 +1000

    fixed vpn protocols - they weren't strings

---
+++ modules/pcoipgw/main.tf
@@ -0,0 +1,90 @@
+#----------------------------------------------------------------
+# This module creates all resources necessary for a PCOIP instance in AWS
+#----------------------------------------------------------------
+
+resource "aws_security_group" "pcoipgw" {
+  name        = "${var.name}"
+  vpc_id      = "${var.vpc_id}"
+  description = "OpenVPN security group"
+
+  tags {
+    Name = "${var.name}"
+  }
+
+  ingress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["${var.vpc_cidr}"]
+    description = "all incoming traffic from vpc"
+  }
+
+  # For OpenVPN Client Web Server & Admin Web UI
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 22
+    to_port     = 22
+    cidr_blocks = ["${var.remote_vpn_ip_cidr}"]
+    description = "ssh"
+  }
+  ingress {
+    protocol    = "tcp"
+    from_port   = 443
+    to_port     = 443
+    cidr_blocks = ["${var.remote_vpn_ip_cidr}"]
+    description = "https"
+  }
+  ingress {
+    protocol    = "udp"
+    from_port   = 1194
+    to_port     = 1194
+    cidr_blocks = ["${var.remote_vpn_ip_cidr}"]
+  }
+  ingress {
+    protocol    = "icmp"
+    from_port   = 8
+    to_port     = 0
+    cidr_blocks = ["${var.remote_vpn_ip_cidr}"]
+    description = "icmp"
+  }
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["${var.remote_vpn_ip_cidr}"]
+    description = "all outgoing traffic"
+  }
+}
+
+resource "aws_instance" "pcoipgw" {
+  ami           = "${var.ami}"
+  instance_type = "${var.instance_type}"
+  key_name      = "${var.key_name}"
+  subnet_id     = "${element(var.public_subnet_ids, count.index)}"
+
+  vpc_security_group_ids = ["${aws_security_group.pcoipgw.id}"]
+
+  tags {
+    Name = "${var.name}"
+  }
+
+  # `admin_user` and `admin_pw` need to be passed in to the appliance through `user_data`, see docs -->
+  # https://docs.openvpn.net/how-to-tutorialsguides/virtual-platforms/amazon-ec2-appliance-ami-quick-start-guide/
+  user_data = <<USERDATA
+USERDATA
+
+  provisioner "remote-exec" {
+    connection {
+      user        = "${var.openvpn_user}"
+      host        = "${self.public_ip}"
+      private_key = "${var.private_key}"
+      timeout     = "10m"
+    }
+
+    inline = [
+      # Sleep 60 seconds until AMI is ready
+      "sleep 60",
+    ]
+  }
+}
