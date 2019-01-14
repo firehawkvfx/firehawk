dc305e32daf61a103d43a1b2d4b96d74d5474f69
Andrew Graham <andrew@firehawkvfx.com>
2019-01-14 16:29:13 +1000

    added gateway type and upload nvidia driver automatically.

--- modules/pcoipgw/main.tf
+++ modules/pcoipgw/main.tf
@@ -67,25 +67,30 @@ resource "aws_security_group" "pcoipgw" {
 }
 
 resource "aws_instance" "pcoipgw" {
-  ami           = "${var.ami}"
-  instance_type = "${var.instance_type}"
-  key_name      = "${var.key_name}"
-  subnet_id     = "${element(var.public_subnet_ids, count.index)}"
+  #instance type and ami are determined by the gateway type variable for if you want a graphical or non graphical instance.
+  ami           = "${lookup(var.ami_map, var.gateway_type)}"
+  instance_type = "${lookup(var.instance_type_map, var.gateway_type)}"
+
+  key_name  = "${var.key_name}"
+  subnet_id = "${element(var.public_subnet_ids, count.index)}"
 
   vpc_security_group_ids = ["${aws_security_group.pcoipgw.id}"]
 
   tags {
-    Name = "${var.name}"
+    Name = "${var.gateway_type}"
   }
 
+  # this segment is not currently configured for centos. user name and pw need to be setup in userdata.
   # `admin_user` and `admin_pw` need to be passed in to the appliance through `user_data`, see docs -->
   # https://docs.openvpn.net/how-to-tutorialsguides/virtual-platforms/amazon-ec2-appliance-ami-quick-start-guide/
   user_data = <<USERDATA
+sudo yum update -y
 USERDATA
 
   provisioner "remote-exec" {
     connection {
-      user        = "${var.user}"
+      #user        = "${var.user}"
+      user        = "centos"
       host        = "${self.public_ip}"
       private_key = "${var.private_key}"
       timeout     = "10m"
@@ -95,5 +100,29 @@ USERDATA
       # Sleep 60 seconds until AMI is ready
       "sleep 60",
     ]
+
+    #will need latest nvidia grid driver and reinstall if you do yum update.  xorg update here will break pciop in centos 7.6
+    #sudo yum update -y
+    #sudo systemctl restart pcoip
+    #sudo /bin/sh NVIDIA-Linux-x86_64-390.96-grid.run --dkms -s --install-libglvnd
+    #sudo dracut -fv
+    #sudo reboot
+  }
+}
+
+#this will upload the latest nvidia driver for an update
+resource "null_resource" remoteExecProvisionerWFolder {
+  provisioner "file" {
+    source      = "${path.module}/gpudriver/NVIDIA-Linux-x86_64-390.96-grid.run"
+    destination = "/home/centos/NVIDIA-Linux-x86_64-390.96-grid.run"
+
+    connection {
+      user        = "centos"
+      host        = "${aws_instance.pcoipgw.public_ip}"
+      private_key = "${var.private_key}"
+      type        = "ssh"
+      agent       = "false"
+      timeout     = "10m"
+    }
   }
 }
