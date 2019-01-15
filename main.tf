fa01b2cd8ca3d7e2b739ad0c7e69f0e8b9245ad2
Andrew Graham <andrew@firehawkvfx.com>
2019-01-15 20:39:32 +1000

    reordered softnas to execute before pcoip

--- main.tf
+++ main.tf
@@ -45,25 +45,6 @@ module "vpn" {
   sleep = "${var.sleep}"
 }
 
-#PCOIP Gateway.  This is a graphical instance that serves as a gateway into the vpc should vpn access fail.
-module "pcoipgw" {
-  source = "./modules/pcoipgw"
-
-  #options for gateway type are centos7 and pcoip
-  gateway_type      = "${var.gateway_type}"
-  vpc_id            = "${module.vpc.vpc_id}"
-  vpc_cidr          = "${module.vpc.vpc_cidr_block}"
-  remote_ip_cidr    = "${var.remote_ip_cidr}"
-  public_subnet_ids = "${module.vpc.public_subnets}"
-
-  #a provided route 53 zone id will be modified to have a subdomain to access vpn.  you will need to manually setup a route 53 zone for a domain with an ssl certificate.
-  key_name    = "${var.key_name}"
-  private_key = "${file("${var.local_key_path}")}"
-
-  #sleep will stop instances to save cost during idle time.
-  sleep = "${var.sleep}"
-}
-
 #A single softnas instance that resides in a private subnet for high performance nfs storage
 module "softnas" {
   source = "./modules/softnas"
@@ -78,6 +59,25 @@ module "softnas" {
   bastion_private_ip          = "${module.pcoipgw.private_ip}"
   volumes                     = "${var.volumes}"
   mounts                      = "${var.mounts}"
+
+  #sleep will stop instances to save cost during idle time.
+  sleep = "${var.sleep}"
+}
+
+#PCOIP Gateway.  This is a graphical instance that serves as a gateway into the vpc should vpn access fail.
+module "pcoipgw" {
+  source = "./modules/pcoipgw"
+
+  #options for gateway type are centos7 and pcoip
+  gateway_type      = "${var.gateway_type}"
+  vpc_id            = "${module.vpc.vpc_id}"
+  vpc_cidr          = "${module.vpc.vpc_cidr_block}"
+  remote_ip_cidr    = "${var.remote_ip_cidr}"
+  public_subnet_ids = "${module.vpc.public_subnets}"
+
+  #a provided route 53 zone id will be modified to have a subdomain to access vpn.  you will need to manually setup a route 53 zone for a domain with an ssl certificate.
+  key_name    = "${var.key_name}"
+  private_key = "${file("${var.local_key_path}")}"
 
   #sleep will stop instances to save cost during idle time.
   sleep = "${var.sleep}"
