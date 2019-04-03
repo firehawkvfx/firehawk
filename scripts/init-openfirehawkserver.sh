b4ccf6c8f3afb6dfd14fd9c646ce515db33dcce3
Andrew Graham <queglay@gmail.com>
2019-04-03 21:37:33 +1000

    permissions on db weren't set.

--- scripts/init-openfirehawkserver.sh
+++ scripts/init-openfirehawkserver.sh
@@ -1,7 +1,8 @@
+echo "openfirehawkserver ip: $TF_VAR_openfirehawkserver"
 cd /vagrant
 ansible-playbook -i ansible/inventory/hosts ansible/init.yaml -v
 ansible-playbook -i ansible/inventory/hosts ansible/newuser_deadline.yaml -v
 ansible-playbook -i ansible/inventory/hosts ansible/openfirehawkserver_houdini.yaml -v
-ansible-playbook -i ansible/inventory/hosts ansible/aws-new-key.yaml
+ansible-playbook -i ansible/inventory/hosts ansible/aws-new-key.yaml -v
 #reboot
 # terraform apply --auto-approve
