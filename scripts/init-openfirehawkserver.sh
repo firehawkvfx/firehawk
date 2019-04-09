424753458bd8459218fb1761f91ba7b658579722
Andrew Graham <queglay@gmail.com>
2019-04-09 11:28:32 +1000

    provision deadline on local workstation

--- scripts/init-openfirehawkserver.sh
+++ scripts/init-openfirehawkserver.sh
@@ -4,5 +4,10 @@ ansible-playbook -i ansible/inventory/hosts ansible/init.yaml -v
 ansible-playbook -i ansible/inventory/hosts ansible/newuser_deadline.yaml -v
 ansible-playbook -i ansible/inventory/hosts ansible/openfirehawkserver_houdini.yaml -v
 ansible-playbook -i ansible/inventory/hosts ansible/aws-new-key.yaml -v
+
+# This provisions the deadline slave and monitor on another local workstation.
+ansible -m ping workstation.firehawkvfx.com -i "$TF_VAR_inventory"
+ansible-playbook -i "$TF_VAR_inventory" ansible/localworkstation-deadlineuser.yaml --tags "onsite-install"
+
 #reboot
 # terraform apply --auto-approve
