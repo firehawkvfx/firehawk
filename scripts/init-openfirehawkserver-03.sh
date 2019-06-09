32f7c3f8a65c890ced737b2f44364b210179e699
Andrew Graham <queglay@gmail.com>
2019-06-09 14:14:50 +1000

    splitup init shell scripts

--- scripts/init-openfirehawkserver-03.sh
+++ scripts/init-openfirehawkserver-03.sh
@@ -1,3 +1,5 @@
+# This stage configures the vpc and vpn.  after this stage, vagrant reload and test ping the private ip of the bastion host to ensure the vpn is working.
+
 # REBOOT required for network interface modes to update.  Then launch terraform
 # exit
 # vagrant reload
@@ -8,7 +10,10 @@
 
 cd /vagrant
 source ./update_vars.sh --dev
+# site mounts will be mounted in cloud
 export TF_VAR_site_mounts=False
+export TF_VAR_softnas_config_mounts_on_local_workstation=False
+export TF_VAR_softnas_storage=False
 terraform apply --auto-approve
-# after first terraform apply, vagrant reload to apply the promisc settings to the NIC.  THIS NEEDS TO BE FIXED OR MOUNTS WONT WORK. you will get an error on the render node/remote workstation
-# in the meantime, after a first install, roll up to softnas first, then reload, and then launch nodes.
+
+# after first terraform apply, vagrant reload to apply the promisc settings to the NIC.  THIS NEEDS TO BE FIXED OR MOUNTS from other systems onsite WONT WORK. you will get an error on the render node/remote workstation
--- scripts/init-openfirehawkserver-05.sh
+++ scripts/init-openfirehawkserver-05.sh
@@ -1,4 +1,4 @@
-# This stage configures softnas, but optionally doesn't not setup any mounts reliant on a vpn.
+# this stage will configure mounts from onsite onto the cloud site, and vice versa.
 
 # vagrant reload
 # vagrant ssh
@@ -8,7 +8,7 @@
 cd /vagrant
 source ./update_vars.sh --dev
 export TF_VAR_softnas_storage=True
-# it is possible the next variables are causing issues when set to false.  verification needed.
 export TF_VAR_site_mounts=True
 export TF_VAR_softnas_config_mounts_on_local_workstation=True
 terraform apply --auto-approve
+#should add a test script at this point to validate vpn connection is established, or licence servers may not work.
