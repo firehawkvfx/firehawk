3467f7a6aa8d45921754f24d1d36190b47030172
Andrew Graham <queglay@gmail.com>
2019-06-17 15:38:45 +1000

    add arg to shutdown

--- scripts/deadline-multi-slave.sh
+++ scripts/deadline-multi-slave.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 
-# This shell script executes a new deadline slave instance for every core present on the system
+# This shell script executes a new deadline slave instance for every core present on the system.  run it once.  
+# slaves should auto start on next reboot if the daemon is configured correctly.
+
+argument="$1"
+
+echo ""
+ARGS=''
+if [[ -z $argument ]] ; then
+  echo "Starting one slave per core."
+else
+  case $argument in
+    -s|--shutdown)
+      ARGS='-shutdown'
+      ;;
+    -r|--remove)
+      REMOVE=True
+      ;;
+    *)
+      raise_error "Unknown argument: ${argument}"
+      return
+      ;;
+  esac
+fi
 
 unset CPUCORES
 unset SLAVECOUNT
@@ -16,5 +38,5 @@ echo "SLAVECOUNT=$SLAVECOUNT"
 for i in $(seq $SLAVECOUNT $END); do 
     digit=$(printf "%02d" $i)
     echo 'deadlineslave -name "i-'$digit'"'
-    /opt/Thinkbox/Deadline10/bin/deadlineslave -name "i-$digit" -nogui &
+    /opt/Thinkbox/Deadline10/bin/deadlineslave -name "i-$digit" -nogui $ARGS &
 done
