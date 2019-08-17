5ec898222060e7dea0b4e1beb4f53a1a93e3fd14
Andrew Graham <queglay@gmail.com>
2019-08-17 16:53:30 +1000

    upgrade vars

--- modules/node_centos/variables.tf
+++ modules/node_centos/variables.tf
@@ -1,15 +1,20 @@
-variable "name" {}
+variable "name" {
+}
 
-variable "vpc_id" {}
+variable "vpc_id" {
+}
 
 #example vpc_cidr "10.0.0.0/16"
-variable "vpc_cidr" {}
+variable "vpc_cidr" {
+}
 
 # remote_ip_cidr is the ip address of the remote host / user intending to connect over vpn. eg '197.125.62.53/32'
 #example "125.254.24.255/32"
-variable "remote_ip_cidr" {}
+variable "remote_ip_cidr" {
+}
 
-variable "remote_subnet_cidr" {}
+variable "remote_subnet_cidr" {
+}
 
 # examples ["subnet-0a7554f56af4d6d0a", "subnet-0257c7f8b1d68b6e4"]
 variable "public_subnet_ids" {
@@ -24,14 +29,16 @@ variable "private_subnets_cidr_blocks" {
   default = []
 }
 
-variable "key_name" {}
+variable "key_name" {
+}
 
 #contents of the my_key.pem file to connect to the instance.
-variable "private_key" {}
+variable "private_key" {
+}
 
 #CentOS Linux 7 x86_64 HVM EBS ENA 1805_01-b7ee8a69-ee97-4a49-9e68-afaee216db2e-ami-77ec9308.4 (ami-d8c21dba)
 variable "ami_map" {
-  type = "map"
+  type = map(string)
 
   default = {
     ap-southeast-2 = "ami-d8c21dba"
@@ -55,15 +62,18 @@ variable "skip_update" {
   default = true
 }
 
-variable "vpn_cidr" {}
+variable "vpn_cidr" {
+}
 
 variable "site_mounts" {
   default = false
 }
 
-variable "region" {}
+variable "region" {
+}
 
-variable "local_key_path" {}
+variable "local_key_path" {
+}
 
 # You may wish to use a custom ami that incorporates your own configuration.  Insert the ami details below if you wish to use this.
 variable "use_custom_ami" {
@@ -73,14 +83,18 @@ variable "use_custom_ami" {
 variable "custom_ami" {
   default = ""
 }
-variable "bastion_ip" {}
 
-variable "houdini_license_server_address" {}
+variable "bastion_ip" {
+}
 
-variable "openfirehawkserver" {}
+variable "houdini_license_server_address" {
+}
+
+variable "openfirehawkserver" {
+}
 
 variable "softnas_private_ip1" {
-  default =[]
+  default = []
 }
 
 variable "softnas_export_path" {
@@ -90,3 +104,4 @@ variable "softnas_export_path" {
 variable "softnas_mount_path" {
   default = "/mnt/softnas/nasvol2"
 }
+
--- modules/pcoipgw/main.tf
+++ modules/pcoipgw/main.tf
@@ -3,19 +3,19 @@
 #----------------------------------------------------------------
 
 resource "aws_security_group" "pcoipgw" {
-  name        = "${var.name}"
-  vpc_id      = "${var.vpc_id}"
+  name        = var.name
+  vpc_id      = var.vpc_id
   description = "Teradici PCOIP security group"
 
-  tags {
-    Name = "${var.name}"
+  tags = {
+    Name = var.name
   }
 
   ingress {
     protocol    = "-1"
     from_port   = 0
     to_port     = 0
-    cidr_blocks = ["${var.vpc_cidr}"]
+    cidr_blocks = [var.vpc_cidr]
     description = "all incoming traffic from vpc"
   }
 
@@ -24,7 +24,7 @@ resource "aws_security_group" "pcoipgw" {
     protocol    = "-1"
     from_port   = 0
     to_port     = 0
-    cidr_blocks = ["${var.remote_ip_cidr}"]
+    cidr_blocks = [var.remote_ip_cidr]
     description = "all incoming traffic from remote access ip"
   }
 
@@ -32,7 +32,7 @@ resource "aws_security_group" "pcoipgw" {
     protocol    = "-1"
     from_port   = 0
     to_port     = 0
-    cidr_blocks = ["${var.vpn_cidr}"]
+    cidr_blocks = [var.vpn_cidr]
     description = "all incoming traffic from remote subnet range"
   }
 
@@ -42,42 +42,42 @@ resource "aws_security_group" "pcoipgw" {
     protocol    = "tcp"
     from_port   = 22
     to_port     = 22
-    cidr_blocks = ["${var.remote_ip_cidr}"]
+    cidr_blocks = [var.remote_ip_cidr]
     description = "ssh"
   }
   ingress {
     protocol    = "tcp"
     from_port   = 443
     to_port     = 443
-    cidr_blocks = ["${var.remote_ip_cidr}"]
+    cidr_blocks = [var.remote_ip_cidr]
     description = "https"
   }
   ingress {
     protocol    = "udp"
     from_port   = 1194
     to_port     = 1194
-    cidr_blocks = ["${var.remote_ip_cidr}"]
+    cidr_blocks = [var.remote_ip_cidr]
   }
 
   ingress {
     protocol    = "udp"
     from_port   = 4172
     to_port     = 4172
-    cidr_blocks = ["${var.remote_ip_cidr}"]
+    cidr_blocks = [var.remote_ip_cidr]
   }
 
   ingress {
     protocol    = "tcp"
     from_port   = 4172
     to_port     = 4172
-    cidr_blocks = ["${var.remote_ip_cidr}"]
+    cidr_blocks = [var.remote_ip_cidr]
   }
-    
+
   ingress {
     protocol    = "icmp"
     from_port   = 8
     to_port     = 0
-    cidr_blocks = ["${var.remote_ip_cidr}"]
+    cidr_blocks = [var.remote_ip_cidr]
     description = "icmp"
   }
   egress {
@@ -89,21 +89,25 @@ resource "aws_security_group" "pcoipgw" {
   }
 }
 
-variable "houdini_license_server_address" {}
+variable "houdini_license_server_address" {
+}
+
 variable "private_subnets_cidr_blocks" {
   default = []
 }
 
-variable "openfirehawkserver" {}
-variable "remote_subnet_cidr" {} 
+variable "openfirehawkserver" {
+}
 
+variable "remote_subnet_cidr" {
+}
 
 resource "aws_security_group" "gateway_centos" {
   name        = "gateway_centos"
-  vpc_id      = "${var.vpc_id}"
+  vpc_id      = var.vpc_id
   description = "Gateway Teradici PCOIP security group"
 
-  tags {
+  tags = {
     Name = "gateway_centos"
   }
 
@@ -111,7 +115,7 @@ resource "aws_security_group" "gateway_centos" {
     protocol    = "-1"
     from_port   = 0
     to_port     = 0
-    cidr_blocks = ["${var.vpc_cidr}"]
+    cidr_blocks = [var.vpc_cidr]
     description = "all incoming traffic from vpc"
   }
 
@@ -120,7 +124,7 @@ resource "aws_security_group" "gateway_centos" {
     protocol    = "-1"
     from_port   = 0
     to_port     = 0
-    cidr_blocks = ["${var.remote_ip_cidr}"]
+    cidr_blocks = [var.remote_ip_cidr]
     description = "all incoming traffic from remote access ip"
   }
 
@@ -128,7 +132,7 @@ resource "aws_security_group" "gateway_centos" {
     protocol    = "-1"
     from_port   = 0
     to_port     = 0
-    cidr_blocks = ["${var.vpn_cidr}"]
+    cidr_blocks = [var.vpn_cidr]
     description = "all incoming traffic from remote subnet range"
   }
 
@@ -154,36 +158,68 @@ resource "aws_security_group" "gateway_centos" {
     protocol    = "tcp"
     from_port   = 22
     to_port     = 22
-    cidr_blocks = ["${var.remote_ip_cidr}"]
-    cidr_blocks = ["${concat(list("${var.remote_subnet_cidr}", "${var.remote_ip_cidr}"), "${var.private_subnets_cidr_blocks}")}"]
+    cidr_blocks = [var.remote_ip_cidr]
+    # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
+    # force an interpolation expression to be interpreted as a list by wrapping it
+    # in an extra set of list brackets. That form was supported for compatibilty in
+    # v0.11, but is no longer supported in Terraform v0.12.
+    #
+    # If the expression in the following list itself returns a list, remove the
+    # brackets to avoid interpretation as a list of lists. If the expression
+    # returns a single list item then leave it as-is and remove this TODO comment.
+    cidr_blocks = concat([var.remote_subnet_cidr, var.remote_ip_cidr], var.private_subnets_cidr_blocks )
     description = "ssh"
   }
   ingress {
     protocol    = "tcp"
     from_port   = 443
     to_port     = 443
-    cidr_blocks = ["${var.remote_ip_cidr}"]
+    cidr_blocks = [var.remote_ip_cidr]
     description = "https"
   }
   ingress {
-    protocol    = "tcp"
-    from_port   = 27100
-    to_port     = 27100
-    cidr_blocks = ["${concat(list("${var.remote_subnet_cidr}"), "${var.private_subnets_cidr_blocks}")}"]
+    protocol  = "tcp"
+    from_port = 27100
+    to_port   = 27100
+    # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
+    # force an interpolation expression to be interpreted as a list by wrapping it
+    # in an extra set of list brackets. That form was supported for compatibilty in
+    # v0.11, but is no longer supported in Terraform v0.12.
+    #
+    # If the expression in the following list itself returns a list, remove the
+    # brackets to avoid interpretation as a list of lists. If the expression
+    # returns a single list item then leave it as-is and remove this TODO comment.
+    cidr_blocks = concat([var.remote_subnet_cidr], var.private_subnets_cidr_blocks)
     description = "DeadlineDB MongoDB"
   }
   ingress {
-    protocol    = "tcp"
-    from_port   = 8080
-    to_port     = 8080
-    cidr_blocks = ["${concat(list("${var.remote_subnet_cidr}"), "${var.private_subnets_cidr_blocks}")}"]
+    protocol  = "tcp"
+    from_port = 8080
+    to_port   = 8080
+    # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
+    # force an interpolation expression to be interpreted as a list by wrapping it
+    # in an extra set of list brackets. That form was supported for compatibilty in
+    # v0.11, but is no longer supported in Terraform v0.12.
+    #
+    # If the expression in the following list itself returns a list, remove the
+    # brackets to avoid interpretation as a list of lists. If the expression
+    # returns a single list item then leave it as-is and remove this TODO comment.
+    cidr_blocks = concat([var.remote_subnet_cidr], var.private_subnets_cidr_blocks)
     description = "Deadline And Deadline RCS"
   }
   ingress {
-    protocol    = "tcp"
-    from_port   = 4433
-    to_port     = 4433
-    cidr_blocks = ["${concat(list("${var.remote_subnet_cidr}"), "${var.private_subnets_cidr_blocks}")}"]
+    protocol  = "tcp"
+    from_port = 4433
+    to_port   = 4433
+    # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
+    # force an interpolation expression to be interpreted as a list by wrapping it
+    # in an extra set of list brackets. That form was supported for compatibilty in
+    # v0.11, but is no longer supported in Terraform v0.12.
+    #
+    # If the expression in the following list itself returns a list, remove the
+    # brackets to avoid interpretation as a list of lists. If the expression
+    # returns a single list item then leave it as-is and remove this TODO comment.
+    cidr_blocks = concat([var.remote_subnet_cidr], var.private_subnets_cidr_blocks)
     description = "Deadline RCS TLS HTTPS"
   }
   ingress {
@@ -204,13 +240,13 @@ resource "aws_security_group" "gateway_centos" {
     protocol    = "udp"
     from_port   = 1194
     to_port     = 1194
-    cidr_blocks = ["${var.remote_ip_cidr}"]
+    cidr_blocks = [var.remote_ip_cidr]
   }
   ingress {
     protocol    = "icmp"
     from_port   = 8
     to_port     = 0
-    cidr_blocks = ["${var.remote_ip_cidr}"]
+    cidr_blocks = [var.remote_ip_cidr]
     description = "icmp"
   }
   egress {
@@ -222,7 +258,6 @@ resource "aws_security_group" "gateway_centos" {
   }
 }
 
-
 # You may wish to use a custom ami of your own creation.  insert the ami details below
 variable "use_custom_ami" {
   default = false
@@ -233,52 +268,53 @@ variable "custom_ami" {
 }
 
 locals {
-  skip_update = "${(var.skip_update || var.use_custom_ami)}"
+  skip_update = var.skip_update || var.use_custom_ami
 }
 
 resource "aws_eip" "pcoipgw_eip" {
-
   vpc      = true
-  instance = "${aws_instance.pcoipgw.id}"
+  instance = aws_instance.pcoipgw.id
 
-  tags {
-    role = "workstation_centos"
+  tags = {
+    role  = "workstation_centos"
     route = "public"
   }
 }
+
 resource "aws_instance" "pcoipgw" {
   #instance type and ami are determined by the gateway type variable for if you want a graphical or non graphical instance.
-  ami           = "${var.use_custom_ami ? var.custom_ami : lookup(var.ami_map, var.gateway_type)}"
-  instance_type = "${lookup(var.instance_type_map, var.gateway_type)}"
+  ami           = var.use_custom_ami ? var.custom_ami : var.ami_map[var.gateway_type]
+  instance_type = var.instance_type_map[var.gateway_type]
 
-  key_name  = "${var.key_name}"
-  subnet_id = "${element(var.public_subnet_ids, count.index)}"
+  key_name  = var.key_name
+  subnet_id = element(var.public_subnet_ids, 0)
 
-  vpc_security_group_ids = ["${aws_security_group.pcoipgw.id}", "${aws_security_group.gateway_centos.id}"]
+  vpc_security_group_ids = [aws_security_group.pcoipgw.id, aws_security_group.gateway_centos.id]
 
   ebs_optimized = true
   root_block_device {
-    volume_size = "30"
-    volume_type = "gp2"
+    volume_size           = "30"
+    volume_type           = "gp2"
     delete_on_termination = true
   }
 
-  tags {
+  tags = {
     Name  = "gateway_centos"
     Route = "public"
-    #Role  = "workstation_centos"
   }
+
+  #Role  = "workstation_centos"
 
   provisioner "remote-exec" {
     connection {
+      type        = "ssh"
       user        = "centos"
-      host        = "${self.public_ip}"
-      private_key = "${var.private_key}"
+      host        = self.public_ip
+      private_key = var.private_key
       timeout     = "10m"
     }
 
     inline = [
-      # Sleep 60 seconds until AMI is ready
       "sleep 60",
     ]
   }
@@ -287,34 +323,38 @@ resource "aws_instance" "pcoipgw" {
   # }
 }
 
-variable "route_zone_id" {}
-variable "public_domain_name" {}
+variable "route_zone_id" {
+}
+
+variable "public_domain_name" {
+}
 
 resource "aws_route53_record" "bastion_record" {
-  zone_id = "${var.route_zone_id}"
+  zone_id = var.route_zone_id
   name    = "gateway.${var.public_domain_name}"
   type    = "A"
   ttl     = 300
-  records = ["${aws_eip.pcoipgw_eip.public_ip}"]
+  records = [aws_eip.pcoipgw_eip.public_ip]
 }
 
 resource "null_resource" "pcoipgw" {
-  count = "${local.skip_update ? 0 : 1}"
+  count = local.skip_update ? 0 : 1
 
-  triggers {
-    instanceid = "${ aws_instance.pcoipgw.id }"
+  triggers = {
+    instanceid = aws_instance.pcoipgw.id
   }
 
   provisioner "remote-exec" {
     connection {
       user                = "centos"
-      host                = "${aws_instance.pcoipgw.private_ip}"
-      bastion_host        = "${var.bastion_ip}"
-      private_key         = "${var.private_key}"
-      bastion_private_key = "${var.private_key}"
+      host                = aws_instance.pcoipgw.private_ip
+      bastion_host        = var.bastion_ip
+      private_key         = var.private_key
+      bastion_private_key = var.private_key
       type                = "ssh"
       timeout             = "10m"
     }
+
     # First we install python remotely via the bastion to bootstrap the instance.  We also need this remote-exec to ensure the host is up.
     inline = ["sleep 10 && set -x && sudo yum install -y python"]
   }
@@ -329,15 +369,16 @@ resource "null_resource" "pcoipgw" {
       ansible-playbook -i ansible/inventory ansible/node-centos-houdini.yaml -v --extra-vars "variable_host=pcoipgw_eip.0 hostname=gateway.${var.public_domain_name} firehawk_sync_source=$TF_VAR_firehawk_sync_source"
       # to recover from yum update breaking pcoip we reinstall the nvidia driver and dracut to fix pcoip.
       ansible-playbook -i ansible/inventory ansible/node-centos-pcoip-recover.yaml -v --extra-vars "variable_host=pcoipgw_eip.0 hostname=gateway.${var.public_domain_name}"
-  EOT
+  
+EOT
+
   }
 
   #after dracut, we reboot the instance locally.  annoyingly, a reboot command will cause a terraform error.
+  #after dracut, we reboot the instance locally.  annoyingly, a reboot command will cause a terraform error.
   provisioner "local-exec" {
-    command = "${var.pcoip_sleep_after_creation ? "aws ec2 stop-instances --instance-ids ${aws_instance.pcoipgw.id}" : "aws ec2 reboot-instances --instance-ids ${aws_instance.pcoipgw.id}"}"
+    command = var.pcoip_sleep_after_creation ? "aws ec2 stop-instances --instance-ids ${aws_instance.pcoipgw.id}" : "aws ec2 reboot-instances --instance-ids ${aws_instance.pcoipgw.id}"
   }
-
-
   #transfer the gpu driver
   # provisioner "file" {
   #   source      = "${path.module}/gpudriver/NVIDIA-Linux-x86_64-390.96-grid.run"
@@ -374,9 +415,10 @@ resource "null_resource" "pcoipgw" {
 }
 
 resource "null_resource" "shutdownpcoipgw" {
-  count = "${var.sleep ? 1 : 0}"
+  count = var.sleep ? 1 : 0
 
   provisioner "local-exec" {
     command = "aws ec2 stop-instances --instance-ids ${aws_instance.pcoipgw.id}"
   }
 }
+
