39341d87cf940cb49d03f070a6cabea5e8633f14
Andrew Graham <queglay@gmail.com>
2019-08-17 16:51:19 +1000

    upgrade vars and lists changes

--- modules/node_centos/main.tf
+++ modules/node_centos/main.tf
@@ -3,19 +3,19 @@
 #----------------------------------------------------------------
 
 resource "aws_security_group" "node_centos" {
-  name        = "${var.name}"
-  vpc_id      = "${var.vpc_id}"
+  name        = var.name
+  vpc_id      = var.vpc_id
   description = "Teradici PCOIP security group"
 
-  tags {
-    Name = "${var.name}"
+  tags = {
+    Name = var.name
   }
 
   ingress {
     protocol    = "-1"
     from_port   = 0
     to_port     = 0
-    cidr_blocks = ["${var.vpc_cidr}"]
+    cidr_blocks = [var.vpc_cidr]
     description = "all incoming traffic from vpc"
   }
 
@@ -24,7 +24,7 @@ resource "aws_security_group" "node_centos" {
     protocol    = "-1"
     from_port   = 0
     to_port     = 0
-    cidr_blocks = ["${var.remote_ip_cidr}"]
+    cidr_blocks = [var.remote_ip_cidr]
     description = "all incoming traffic from remote access ip"
   }
 
@@ -32,18 +32,17 @@ resource "aws_security_group" "node_centos" {
     protocol    = "-1"
     from_port   = 0
     to_port     = 0
-    cidr_blocks = ["${var.vpn_cidr}"]
+    cidr_blocks = [var.vpn_cidr]
     description = "all incoming traffic from remote subnet range vpn dhcp"
   }
-  
+
   ingress {
     protocol    = "-1"
     from_port   = 0
     to_port     = 0
-    cidr_blocks = ["${var.remote_subnet_cidr}"]
+    cidr_blocks = [var.remote_subnet_cidr]
     description = "all incoming traffic from remote subnet range"
   }
-  
 
   ingress {
     protocol    = "-1"
@@ -67,36 +66,68 @@ resource "aws_security_group" "node_centos" {
     protocol    = "tcp"
     from_port   = 22
     to_port     = 22
-    cidr_blocks = ["${var.remote_ip_cidr}"]
-    cidr_blocks = ["${concat(list("${var.remote_subnet_cidr}", "${var.remote_ip_cidr}"), "${var.private_subnets_cidr_blocks}")}"]
+    cidr_blocks = [var.remote_ip_cidr]
+    # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
+    # force an interpolation expression to be interpreted as a list by wrapping it
+    # in an extra set of list brackets. That form was supported for compatibilty in
+    # v0.11, but is no longer supported in Terraform v0.12.
+    #
+    # If the expression in the following list itself returns a list, remove the
+    # brackets to avoid interpretation as a list of lists. If the expression
+    # returns a single list item then leave it as-is and remove this TODO comment.
+    cidr_blocks = concat([var.remote_subnet_cidr, var.remote_ip_cidr], var.private_subnets_cidr_blocks)
     description = "ssh"
   }
   ingress {
     protocol    = "tcp"
     from_port   = 443
     to_port     = 443
-    cidr_blocks = ["${var.remote_ip_cidr}"]
+    cidr_blocks = [var.remote_ip_cidr]
     description = "https"
   }
   ingress {
-    protocol    = "tcp"
-    from_port   = 27100
-    to_port     = 27100
-    cidr_blocks = ["${concat(list("${var.remote_subnet_cidr}"), "${var.private_subnets_cidr_blocks}")}"]
+    protocol  = "tcp"
+    from_port = 27100
+    to_port   = 27100
+    # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
+    # force an interpolation expression to be interpreted as a list by wrapping it
+    # in an extra set of list brackets. That form was supported for compatibilty in
+    # v0.11, but is no longer supported in Terraform v0.12.
+    #
+    # If the expression in the following list itself returns a list, remove the
+    # brackets to avoid interpretation as a list of lists. If the expression
+    # returns a single list item then leave it as-is and remove this TODO comment.
+    cidr_blocks = concat([var.remote_subnet_cidr], var.private_subnets_cidr_blocks)
     description = "DeadlineDB MongoDB"
   }
   ingress {
-    protocol    = "tcp"
-    from_port   = 8080
-    to_port     = 8080
-    cidr_blocks = ["${concat(list("${var.remote_subnet_cidr}"), "${var.private_subnets_cidr_blocks}")}"]
+    protocol  = "tcp"
+    from_port = 8080
+    to_port   = 8080
+    # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
+    # force an interpolation expression to be interpreted as a list by wrapping it
+    # in an extra set of list brackets. That form was supported for compatibilty in
+    # v0.11, but is no longer supported in Terraform v0.12.
+    #
+    # If the expression in the following list itself returns a list, remove the
+    # brackets to avoid interpretation as a list of lists. If the expression
+    # returns a single list item then leave it as-is and remove this TODO comment.
+    cidr_blocks = concat([var.remote_subnet_cidr], var.private_subnets_cidr_blocks)
     description = "Deadline And Deadline RCS"
   }
   ingress {
-    protocol    = "tcp"
-    from_port   = 4433
-    to_port     = 4433
-    cidr_blocks = ["${concat(list("${var.remote_subnet_cidr}"), "${var.private_subnets_cidr_blocks}")}"]
+    protocol  = "tcp"
+    from_port = 4433
+    to_port   = 4433
+    # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
+    # force an interpolation expression to be interpreted as a list by wrapping it
+    # in an extra set of list brackets. That form was supported for compatibilty in
+    # v0.11, but is no longer supported in Terraform v0.12.
+    #
+    # If the expression in the following list itself returns a list, remove the
+    # brackets to avoid interpretation as a list of lists. If the expression
+    # returns a single list item then leave it as-is and remove this TODO comment.
+    cidr_blocks = concat([var.remote_subnet_cidr], var.private_subnets_cidr_blocks)
     description = "Deadline RCS TLS HTTPS"
   }
   ingress {
@@ -117,13 +148,13 @@ resource "aws_security_group" "node_centos" {
     protocol    = "udp"
     from_port   = 1194
     to_port     = 1194
-    cidr_blocks = ["${var.remote_ip_cidr}"]
+    cidr_blocks = [var.remote_ip_cidr]
   }
   ingress {
     protocol    = "icmp"
     from_port   = 8
     to_port     = 0
-    cidr_blocks = ["${var.remote_ip_cidr}"]
+    cidr_blocks = [var.remote_ip_cidr]
     description = "icmp"
   }
   egress {
@@ -136,22 +167,24 @@ resource "aws_security_group" "node_centos" {
 }
 
 variable "provision_softnas_volumes" {
-  default =[]
+  default = []
 }
 
 resource "null_resource" "dependency_softnas_and_bastion" {
-  triggers {
-    softnas_private_ip1 = "${join(",", var.softnas_private_ip1)}"
-    bastion_ip = "${var.bastion_ip}"
-    provision_softnas_volumes = "${join(",", var.provision_softnas_volumes)}"
+  triggers = {
+    softnas_private_ip1       = join(",", var.softnas_private_ip1)
+    bastion_ip                = var.bastion_ip
+    provision_softnas_volumes = join(",", var.provision_softnas_volumes)
   }
 }
+
 resource "aws_instance" "node_centos" {
-  count = "${var.site_mounts ? 1 : 0}"
-  depends_on = ["null_resource.dependency_softnas_and_bastion"]
+  count      = var.site_mounts ? 1 : 0
+  depends_on = [null_resource.dependency_softnas_and_bastion]
+
   #instance type and ami are determined by the gateway type variable for if you want a graphical or non graphical instance.
-  ami           = "${var.use_custom_ami ? var.custom_ami : lookup(var.ami_map, var.region)}"
-  instance_type = "${var.instance_type}"
+  ami           = var.use_custom_ami ? var.custom_ami : var.ami_map[var.region]
+  instance_type = var.instance_type
 
   #ebs_optimized = true
 
@@ -159,10 +192,10 @@ resource "aws_instance" "node_centos" {
     volume_size = "16"
     volume_type = "standard"
   }
-  key_name               = "${var.key_name}"
-  subnet_id              = "${element(var.private_subnet_ids, count.index)}"
-  vpc_security_group_ids = ["${aws_security_group.node_centos.id}"]
-  tags {
+  key_name               = var.key_name
+  subnet_id              = element(var.private_subnet_ids, count.index)
+  vpc_security_group_ids = [aws_security_group.node_centos.id]
+  tags = {
     Name  = "node_centos"
     Route = "private"
     Role  = "node_centos"
@@ -170,33 +203,34 @@ resource "aws_instance" "node_centos" {
 }
 
 resource "null_resource" "provision_node_centos" {
-  count = "${var.site_mounts ? 1 : 0}"
-  depends_on = ["aws_instance.node_centos"]
+  count      = var.site_mounts ? 1 : 0
+  depends_on = [aws_instance.node_centos]
 
-  triggers {
-    instanceid = "${ aws_instance.node_centos.id }"
+  triggers = {
+    instanceid = aws_instance.node_centos[0].id
   }
-  
+
   provisioner "local-exec" {
-    command = "aws ec2 start-instances --instance-ids ${aws_instance.node_centos.id}"
+    command = "aws ec2 start-instances --instance-ids ${aws_instance.node_centos[0].id}"
   }
 
   provisioner "remote-exec" {
     connection {
       user                = "centos"
-      host                = "${aws_instance.node_centos.private_ip}"
-      bastion_host        = "${var.bastion_ip}"
-      private_key         = "${var.private_key}"
-      bastion_private_key = "${var.private_key}"
+      host                = aws_instance.node_centos[0].private_ip
+      bastion_host        = var.bastion_ip
+      private_key         = var.private_key
+      bastion_private_key = var.private_key
       type                = "ssh"
       timeout             = "10m"
     }
+
     # First we install python remotely via the bastion to bootstrap the instance.  We also need this remote-exec to ensure the host is up.
     inline = [
       "sleep 10",
       "set -x",
       "sudo yum install -y python",
-      "ssh-keyscan ${aws_instance.node_centos.private_ip}"
+      "ssh-keyscan ${aws_instance.node_centos[0].private_ip}",
     ]
   }
 
@@ -204,7 +238,7 @@ resource "null_resource" "provision_node_centos" {
     command = <<EOT
       set -x
       cd /vagrant
-      ansible-playbook -i ansible/inventory ansible/ssh-add-private-host.yaml -v --extra-vars "private_ip=${aws_instance.node_centos.private_ip} bastion_ip=${var.bastion_ip}"
+      ansible-playbook -i ansible/inventory ansible/ssh-add-private-host.yaml -v --extra-vars "private_ip=${aws_instance.node_centos[0].private_ip} bastion_ip=${var.bastion_ip}"
       ansible-playbook -i ansible/inventory ansible/node-centos-init-users.yaml -v
       ansible-playbook -i ansible/inventory ansible/aws-cli-ec2-install.yaml -v --extra-vars "variable_host=role_node_centos variable_user=deadlineuser"
       ansible-playbook -i ansible/inventory ansible/aws-cli-ec2-install.yaml -v --extra-vars "variable_host=role_node_centos variable_user=centos"
@@ -212,39 +246,42 @@ resource "null_resource" "provision_node_centos" {
       ansible-playbook -i ansible/inventory ansible/localworkstation-deadlineuser.yaml --tags "onsite-install" --extra-vars "variable_host=role_node_centos variable_user=centos"
       ansible-playbook -i ansible/inventory ansible/node-centos-houdini.yaml -v --extra-vars "sesi_username=$TF_VAR_sesi_username sesi_password=$TF_VAR_sesi_password houdini_build=$TF_VAR_houdini_build firehawk_sync_source=$TF_VAR_firehawk_sync_source"
       ansible-playbook -i ansible/inventory ansible/node-centos-ffmpeg.yaml -v
-  EOT
+  
+EOT
+
   }
 }
 
 resource "random_id" "ami_unique_name" {
-  count = "${var.site_mounts ? 1 : 0}"
+  count = var.site_mounts ? 1 : 0
   keepers = {
     # Generate a new id each time we switch to a new instance id
-    ami_id = "${aws_instance.node_centos.id}"
+    ami_id = aws_instance.node_centos[0].id
   }
   byte_length = 8
 }
 
 resource "aws_ami_from_instance" "node_centos" {
-  count = "${var.site_mounts ? 1 : 0}"
-  depends_on         = ["null_resource.provision_node_centos"]
-  name               = "node_centos_houdini_${aws_instance.node_centos.id}_${random_id.ami_unique_name.hex}"
-  source_instance_id = "${aws_instance.node_centos.id}"
+  count              = var.site_mounts ? 1 : 0
+  depends_on         = [null_resource.provision_node_centos]
+  name               = "node_centos_houdini_${aws_instance.node_centos[0].id}_${random_id.ami_unique_name[0].hex}"
+  source_instance_id = aws_instance.node_centos[0].id
 }
 
 #wakeup a node after sleep
 resource "null_resource" "start-node" {
-  count = "${var.sleep == false && var.site_mounts ? 1 : 0}"
+  count = var.sleep == false && var.site_mounts ? 1 : 0
 
   provisioner "local-exec" {
-    command = "aws ec2 start-instances --instance-ids ${aws_instance.node_centos.id}"
+    command = "aws ec2 start-instances --instance-ids ${aws_instance.node_centos[0].id}"
   }
 }
 
 resource "null_resource" "shutdown-node" {
-  count = "${var.sleep && var.site_mounts ? 1 : 0}"
+  count = var.sleep && var.site_mounts ? 1 : 0
 
   provisioner "local-exec" {
-    command = "aws ec2 stop-instances --instance-ids ${aws_instance.node_centos.id}"
+    command = "aws ec2 stop-instances --instance-ids ${aws_instance.node_centos[0].id}"
   }
 }
+
--- modules/node_centos/outputs.tf
+++ modules/node_centos/outputs.tf
@@ -1,7 +1,8 @@
 output "private_ip" {
-  value = "${aws_instance.node_centos.*.private_ip}"
+  value = aws_instance.node_centos.*.private_ip
 }
 
 output "public_ip" {
-  value = "${aws_instance.node_centos.*.public_ip}"
+  value = aws_instance.node_centos.*.public_ip
 }
+
