6cdcf882f492f27e28e6c8280da90a7e6eb6c21e
Andrew Graham <queglay@gmail.com>
2019-08-22 14:31:33 +1000

    standardise houdini install for licence server

--- scripts/init-openfirehawkserver-02-ssh-routes.sh
+++ scripts/init-openfirehawkserver-02-ssh-routes.sh
@@ -41,7 +41,10 @@ ansible-playbook -i ansible/inventory/hosts ansible/node-centos-mounts.yaml --ex
 # ssh will be killed from the previous script because users were added to a new group and this will not update unless your ssh session is restarted.
 # login again and continue...
 
-ansible-playbook -i ansible/inventory/hosts ansible/openfirehawkserver_houdini.yaml
+# ansible-playbook -i ansible/inventory/hosts ansible/openfirehawkserver_houdini.yaml
+# install houdini with the same procedure as on render nodes and workstations, and initialise the licence server on this system.
+ansible-playbook -i "$TF_VAR_inventory" ansible/node-centos-houdini.yaml -v --extra-vars "variable_host=localhost variable_user=vagrant houdini_install_type=server" --skip-tags "sync_scripts"
+
 ansible-playbook -i ansible/inventory/hosts ansible/aws-new-key.yaml
 # configure routes to opposite environment for licence server to communicate if in dev environment
 ansible-playbook -i ansible/inventory ansible/ansible-control-update-routes.yaml
