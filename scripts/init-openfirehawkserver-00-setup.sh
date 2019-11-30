c531bfab876132005d9426b5e1c80e0e6771edc0
Andrew Graham <queglay@gmail.com>
2019-11-30 19:44:20 +1030

    update docs and lines on initialisation

--- scripts/init-openfirehawkserver-00-setup.sh
+++ scripts/init-openfirehawkserver-00-setup.sh
@@ -5,8 +5,35 @@
 # if values dont exist, the user will be prompted to initialise a value.
 # if values are already defined in the encrypted settings they will be skipped.
 
+clear
+
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-echo $SCRIPTDIR
+printf "\n...checking scripts directory at $SCRIPTDIR\n\n"
+
+PS3='Please enter your choice: '
+options=("Initialise" "Update" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Initialise")
+            printf "\nInitialise / Reinitialise: Existing secrets will be archived and you will be prompted for new values to replace old ones\n\n"
+            replace_all=true
+            break
+            ;;
+        "Update")
+            printf "\nUpdate: Only new keys in the template that you have not set values for will prompt you for a value.\n\n"
+            replace_all=false
+            break
+            ;;
+        "Quit")
+            echo "You selected $REPLY to $opt"
+            exit
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
+
+read -p 'Press enter to continue'
 
 # IFS will allow for lop to iterate over lines instead of words seperated by new line char
 IFS='
@@ -22,18 +49,19 @@ rm $output
 #consider erase output?
 #rm $output
 
-replace_all=false
+
 
 if [[ ! $TF_VAR_envtier ]]; then
-    echo "No Environment has been initialised.  Assuming first time installation.  if this is incorrect, initialise variables with"
+    echo "No Environment has been initialised.  Assuming first time installation.  if this is incorrect, initialise variables first with:"
     echo "source ./update_vars.sh"
+    printf "\n...propogating the configuration in to secrets file\n\n"
 fi
 
 for i in `(cat $input | sed 's/^$/###/')`
 do
     if [[ "$i" =~ ^.*=insertvalue$ ]]
     then
-        # if insertvalue in line, then prompt user for value. 
+        # if insertvalue is in line from template, then prompt user for value. 
         # compare with current env var. if initialised, use env var and dont ask user.
         command="echo \$${i%%=*}"
         current_value=`eval $command`
