4bdc37cc959eaf4ea01d232613af41215ca8f2af
Andrew Graham <queglay@gmail.com>
2019-12-22 20:08:52 +1030

    cleanup comments

--- scripts/init-openfirehawkserver-07-cloudnodes-localmounts.sh
+++ scripts/init-openfirehawkserver-07-cloudnodes-localmounts.sh
@@ -33,24 +33,14 @@ fi
 echo 'Use vagrant reload and vagrant ssh after executing each .sh script'
 echo "openfirehawkserver ip: $TF_VAR_openfirehawkserver"
 
-# this stage will configure mounts from onsite onto the cloud site, and vice versa.
-
-# vagrant reload
-# vagrant ssh
-
-# test the vpn buy logging into softnas and ping another system on your local network.
-
-# export TF_VAR_softnas_storage=true
-# export TF_VAR_site_mounts=true
-# export TF_VAR_remote_mounts_on_local=false
 
 config_override=$(to_abs_path $TF_VAR_firehawk_path/../secrets/config-override-$TF_VAR_envtier)
 echo "...Config Override path $config_override"
-echo '...Site mounts will not be mounted in cloud.  currently this will disable provisioning any render node or remote workstation until vpn is confirmed to function after this step'
+echo '...Configure Softnas instance enabled'
 sudo sed -i 's/^TF_VAR_softnas_storage=.*$/TF_VAR_softnas_storage=true/' $config_override
-echo '...Softnas nfs exports will not be mounted on local site'
+echo '...Site mounts will be mounted in cloud'
 sudo sed -i 's/^TF_VAR_site_mounts=.*$/TF_VAR_site_mounts=true/' $config_override
-echo 'on first apply, dont create softnas instance until vpn is working'
+echo '...Softnas nfs exports will not be mounted on local site'
 sudo sed -i 's/^TF_VAR_remote_mounts_on_local=.*$/TF_VAR_remote_mounts_on_local=false/' $config_override
 echo "...Sourcing config override"
 source $TF_VAR_firehawk_path/update_vars.sh --$TF_VAR_envtier --var-file config-override
