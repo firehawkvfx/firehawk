afb02f12d0e548a1c9404c381f9e8c81f6e1c726
Andrew Graham <queglay@gmail.com>
2019-12-28 16:56:09 +1030

    fix exit test

--- scripts/exit_test.sh
+++ scripts/exit_test.sh
@@ -2,11 +2,12 @@
 
 # This script only defines the function to test if the last bash line had a non zero exit code.  it is used to interrupt terraform if local-exec ansible scripts fail.
 
+RED='\033[0;31m' # Red Text
+GREEN='\033[0;32m' # Green Text
+BLUE='\033[0;34m' # Blue Text
+NC='\033[0m' # No Color
+
 exit_test () {
-    RED='\033[0;31m' # Red Text
-    GREEN='\033[0;32m' # Green Text
-    BLUE='\033[0;34m' # Blue Text
-    NC='\033[0m' # No Color
     if [ $? -eq 0 ]; then
         printf "\n${GREEN}Playbook Succeeded${NC}\n"
     else
