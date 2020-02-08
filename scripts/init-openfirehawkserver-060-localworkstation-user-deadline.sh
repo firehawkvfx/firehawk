cf541ac4fa78a6d8c5d89719f25072df58732f87
Andrew Graham <queglay@gmail.com>
2020-02-08 21:25:25 +1030

    remove note

--- scripts/init-openfirehawkserver-060-localworkstation-user-deadline.sh
+++ scripts/init-openfirehawkserver-060-localworkstation-user-deadline.sh
@@ -58,9 +58,6 @@ echo "openfirehawkserver ip: $TF_VAR_openfirehawkserver"
 
 ansible-playbook -i "$TF_VAR_inventory" ansible/deadline-db-check.yaml -v; exit_test
 
-# This stage configures deadline on the local workstation
-# REBOOT required for network interface modes to update.
-
 # configure deadline on the local workstation with the keys from this install to run deadline slave and monitor
 ansible-playbook -i "$TF_VAR_inventory" ansible/deadline-worker-install.yaml --tags "onsite-install" --extra-vars "variable_host=workstation1 variable_user=deadlineuser variable_connect_as_user=deployuser"; exit_test
 
