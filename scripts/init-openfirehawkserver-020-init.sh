c8632a3f9fd0e2f38494e6aacb3265a9d972257c
Andrew Graham <queglay@gmail.com>
2020-02-08 21:25:36 +1030

    correct note

--- scripts/init-openfirehawkserver-020-init.sh
+++ scripts/init-openfirehawkserver-020-init.sh
@@ -73,7 +73,7 @@ ansible-playbook -i "$TF_VAR_inventory" ansible/ssh-add-private-host.yaml -v --e
 echo "Add this host and address to ansible inventory"
 ansible-playbook -i "$TF_VAR_inventory" ansible/inventory-add.yaml -v --extra-vars "host_name=firehawkgateway host_ip=$TF_VAR_openfirehawkserver group_name=role_gateway insert_ssh_key_string=ansible_ssh_private_key_file=$TF_VAR_general_use_ssh_key"; exit_test
 
-# Now this will init the deployuser on the workstation.  the deployuser will become the primary user with ssh access.  After this point the deployuser user could be destroyed for further hardening.
+# Now this will init the deployuser on the workstation.  the deployuser will become the primary user with ssh access.
 ansible-playbook -i "$TF_VAR_inventory" ansible/newuser_sshuser.yaml -v --extra-vars "variable_host=firehawkgateway user_inituser_name=deployuser user_inituser_pw='' ansible_ssh_private_key_file=/deployuser/.vagrant/machines/firehawkgateway/virtualbox/private_key"; exit_test
 echo "Ping the host as deployuser..."
 ansible -m ping firehawkgateway -i "$TF_VAR_inventory" --private-key=$TF_VAR_general_use_ssh_key -u deployuser --become; exit_test
