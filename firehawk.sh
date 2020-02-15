f8b9f0a526ea0e99f74e824fb1e43e983d9d45cf
Andrew Graham <queglay@gmail.com>
2020-02-15 20:36:01 +1030

    Prototype password passthrough to vm via ssh and expect

---
+++ firehawk.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# echo "Enter Secrets Decryption Password..."
+echo -n Password: 
+read -s password
+
+printf "\nRunning in ansiblecontrol with $1..."
+
+vagrant up
+
+hostname=$(vagrant ssh-config ansiblecontrol | grep -Po '.*HostName\ \K(\d*.\d*.\d*.\d*)')
+port=$(vagrant ssh-config ansiblecontrol | grep -Po '.*Port\ \K(\d*)')
+
+# use expect to pipe through the password aquired initially.
+./scripts/expect-firehawk.sh $hostname $port $1 $password
