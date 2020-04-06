2b7b2fd7d2734308c6659f7d891b9db3f9705309
Andrew Graham <queglay@gmail.com>
2020-04-06 22:40:34 +0930

    add args for workstation and user

--- modules/firehawk_init/main.tf
+++ modules/firehawk_init/main.tf
@@ -225,7 +225,7 @@ resource "null_resource" "install-houdini-local-workstation" {
 
       if [[ $TF_VAR_houdini_test_connection == true ]]; then
         # last step before building ami we run a unit test to ensure houdini runs
-        ansible-playbook -i "$TF_VAR_inventory" ansible/node-centos-houdini-unit-test.yaml -v --extra-vars "sesi_username=$TF_VAR_sesi_username sesi_password=$TF_VAR_sesi_password houdini_build=$TF_VAR_houdini_build firehawk_sync_source=$TF_VAR_firehawk_sync_source execute=true"; exit_test
+        ansible-playbook -i "$TF_VAR_inventory" ansible/node-centos-houdini-unit-test.yaml -v --extra-vars "variable_host=workstation1 variable_user=deadlineuser firehawk_sync_source=$TF_VAR_firehawk_sync_source execute=true"; exit_test
       fi
 EOT
 }
