88fe443c2cb9b7bcaaa5bd8c62b6a333fb8c5271
Andrew Graham <queglay@gmail.com>
2020-05-02 21:19:40 +0930

    don't use include_role:

--- scripts/ci-set-deploy-cloud-nodes.sh
+++ scripts/ci-set-deploy-cloud-nodes.sh
@@ -20,5 +20,5 @@ sed -i 's/^TF_VAR_remote_mounts_on_local=.*$/TF_VAR_remote_mounts_on_local=true/
 sed -i 's/^TF_VAR_provision_deadline_spot_plugin=.*$/TF_VAR_provision_deadline_spot_plugin=true/' $config_override # Don't provision the deadline spot plugin for this stage
 sed -i 's/^TF_VAR_install_houdini=.*$/TF_VAR_install_houdini=true/' $config_override # install houdini
 sed -i 's/^TF_VAR_install_deadline=.*$/TF_VAR_install_deadline=true/' $config_override # install deadline
-sed -i 's/^TF_VAR_tf_destroy_before_deploy=.*$/TF_VAR_tf_destroy_before_deploy=true/' $config_override # destroy before deploy
-sed -i 's/^TF_VAR_taint_single=.*$/TF_VAR_taint_single=(module.node.aws_instance.node_centos[0])/' $config_override # taint vpn
+sed -i 's/^TF_VAR_tf_destroy_before_deploy=.*$/TF_VAR_tf_destroy_before_deploy=false/' $config_override # destroy before deploy
+sed -i 's/^TF_VAR_taint_single=.*$/TF_VAR_taint_single=(module.node.null_resource.mounts_and_houdini_test[0])/' $config_override # taint vpn
