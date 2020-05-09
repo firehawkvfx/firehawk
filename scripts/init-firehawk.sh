e50074710e70ff488bc7b14d27476f226c09f10b
Andrew Graham <queglay@gmail.com>
2020-05-10 01:57:02 +0930

    reduce verbosity [ci skip]

--- scripts/init-firehawk.sh
+++ scripts/init-firehawk.sh
@@ -183,7 +183,7 @@ else
     aws iam list-users
     echo ""
 
-    ansible-playbook -vv -i "$TF_VAR_inventory" ansible/aws-new-key.yaml; exit_test # ensure aws key is present.
+    ansible-playbook -i "$TF_VAR_inventory" ansible/aws-new-key.yaml; exit_test # ensure aws key is present.
 
     echo "...Terraform refresh"
     if terraform refresh -lock=false; then
