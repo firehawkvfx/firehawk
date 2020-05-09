810b22b371cbe5fee5e00c005c37483e8c5e7287
Andrew Graham <queglay@gmail.com>
2020-05-09 23:59:07 +0930

    test update method change [fast]

--- scripts/init-firehawk.sh
+++ scripts/init-firehawk.sh
@@ -174,7 +174,7 @@ else
     # 
     echo "current TF_VAR_local_key_path: $TF_VAR_local_key_path"
     touch $TF_VAR_local_key_path
-    
+
     echo "...Terraform refresh"
     if terraform refresh -lock=false; then
       echo "...Terraform destroy"
@@ -193,15 +193,15 @@ else
 
     ### Initialisation for new resources occur after a destroy operation, since the infra is garunteed to be new after his point.
     sed -i "s/^TF_VAR_active_pipeline=.*$/TF_VAR_active_pipeline=${TF_VAR_CI_PIPELINE_ID}/" $config_override # ...Enable the vpc.
-    source ./update_vars.sh --$TF_VAR_envtier --init
+    source ./update_vars.sh --$TF_VAR_envtier
     echo "TF_VAR_active_pipeline: $TF_VAR_active_pipeline"
     sed -i "s/^TF_VAR_key_name_${TF_VAR_envtier}=.*$/TF_VAR_key_name_${TF_VAR_envtier}=my_key_pair_pipeid${TF_VAR_active_pipeline}_${TF_VAR_envtier}/" $config_override
-    source ./update_vars.sh --$TF_VAR_envtier --init
+    source ./update_vars.sh --$TF_VAR_envtier
     echo "TF_VAR_key_name: $TF_VAR_key_name"
     key_path=/secrets/keys/${TF_VAR_key_name}.pem
     echo "key_path: $key_path"
     sed -i "s~^TF_VAR_local_key_path_${TF_VAR_envtier}=.*$~TF_VAR_local_key_path_${TF_VAR_envtier}=${key_path}~" $config_override
-    source ./update_vars.sh --$TF_VAR_envtier --init
+    source ./update_vars.sh --$TF_VAR_envtier
     echo "TF_VAR_local_key_path: $TF_VAR_local_key_path"
     ansible-playbook -i "$TF_VAR_inventory" ansible/aws-new-key.yaml; exit_test # ensure an aws pem key exists for ssh into cloud nodes
     ### End init new infra
