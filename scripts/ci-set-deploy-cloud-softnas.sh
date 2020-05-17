a457b2fd561e82645c2b865f72f89c09a539b4f5
Andrew Graham <queglay@gmail.com>
2020-05-17 12:19:28 +0930

    use dict as tags

--- scripts/ci-set-deploy-cloud-softnas.sh
+++ scripts/ci-set-deploy-cloud-softnas.sh
@@ -25,6 +25,6 @@ sed -i 's/^TF_VAR_install_deadline_db=.*$/TF_VAR_install_deadline_db=false/' $co
 sed -i 's/^TF_VAR_install_deadline_rcs=.*$/TF_VAR_install_deadline_rcs=false/' $config_override # install deadline
 sed -i 's/^TF_VAR_install_deadline_worker=.*$/TF_VAR_install_deadline_worker=false/' $config_override # install deadline
 # sed -i 's/^TF_VAR_taint_single=.*$/TF_VAR_taint_single=(module.softnas.aws_instance.softnas1[0] module.softnas.aws_network_interface.nas1eth0[0])/' $config_override # taint 
-sed -i 's/^TF_VAR_taint_single=.*$/TF_VAR_taint_single=(module.softnas.null_resource.provision_softnas_volumes[0])/' $config_override # taint 
+# sed -i 's/^TF_VAR_taint_single=.*$/TF_VAR_taint_single=(module.softnas.null_resource.provision_softnas_volumes[0])/' $config_override # taint 
 sed -i 's/^TF_VAR_allow_prebuilt_softnas_ami=.*$/TF_VAR_allow_prebuilt_softnas_ami=true/' $config_override # allow use of a prebuilt ami with the update if it exists
 sed -i 's/^TF_VAR_tf_destroy_before_deploy=.*$/TF_VAR_tf_destroy_before_deploy=true/' $config_override # destroy before deploy
