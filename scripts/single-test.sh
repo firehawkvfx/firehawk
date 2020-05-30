ea4d999af527c6ba1556963a68ddfe7d81fa9b78
Andrew Graham <queglay@gmail.com>
2020-05-30 13:47:24 +0930

    fix var for current account id

--- scripts/single-test.sh
+++ scripts/single-test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ansible-playbook -i ../secrets/dev/inventory ansible/ansible_collections/firehawkvfx/houdini/houdini_module.yaml -v --extra-vars 'variable_host=workstation1 variable_user=deadlineuser variable_connect_as_user=deployuser' --tags install_houdini --skip-tags sync_scripts
+ansible-playbook -i ../secrets/dev/inventory ansible/ansible_collections/firehawkvfx/houdini/houdini_unit_test.yaml -v --extra-vars 'variable_host=workstation1 variable_user=deadlineuser variable_connect_as_user=deployuser execute=true'
