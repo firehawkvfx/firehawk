8043230210f72bc435e6f5caae2d46b450d1d32b
Andrew Graham <queglay@gmail.com>
2020-05-30 19:42:21 +0930

    show TF_VAR_firehawk_path

--- scripts/single-test.sh
+++ scripts/single-test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+echo "TF_VAR_firehawk_path: $TF_VAR_firehawk_path"
 echo "TF_VAR_secrets_path: $TF_VAR_secrets_path"
 ansible-playbook -i ../secrets/dev/inventory ansible/ansible_collections/firehawkvfx/softnas/linux_volume_mounts.yaml --extra-vars 'variable_host=firehawkgateway variable_user=deployuser softnas_hosts=none' --tags local_install_onsite_mounts
