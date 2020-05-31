9a368c3107bdb42e0aee0393f062213e9f4a7583
Andrew Graham <queglay@gmail.com>
2020-05-31 18:26:30 +0930

    test db

--- scripts/single-test.sh
+++ scripts/single-test.sh
@@ -3,4 +3,4 @@
 echo "TF_VAR_firehawk_path: $TF_VAR_firehawk_path"
 echo "TF_VAR_secrets_path: $TF_VAR_secrets_path"
 
-ansible-playbook -i ../secrets/dev/inventory ansible/ansible_collections/firehawkvfx/houdini/houdini_module.yaml -v --extra-vars 'variable_host=workstation1 variable_user=deadlineuser variable_connect_as_user=deployuser' --tags install_deadline_db --skip-tags sync_scripts
+ansible-playbook -i ../secrets/dev/inventory ansible/deadline-db-check.yaml -v
