81f43eea4f795fd365205ab309ede9f117492508
Andrew Graham <queglay@gmail.com>
2020-05-31 18:33:13 +0930

    no ssh key test

--- scripts/single-test.sh
+++ scripts/single-test.sh
@@ -3,4 +3,4 @@
 echo "TF_VAR_firehawk_path: $TF_VAR_firehawk_path"
 echo "TF_VAR_secrets_path: $TF_VAR_secrets_path"
 
-ansible-playbook -i ../secrets/dev/inventory ansible/deadline-db-check.yaml -v
+ansible-playbook -i ../secrets/dev/inventory ansible/deadline-db-check.yaml -vvv
