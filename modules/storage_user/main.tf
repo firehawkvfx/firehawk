0fe7078527beeaf3a72ac536b662f4e6ab4ae75d
Andrew Graham <queglay@gmail.com>
2020-06-06 22:57:22 +0930

    fix language error

--- modules/storage_user/main.tf
+++ modules/storage_user/main.tf
@@ -2,7 +2,7 @@
 
 
 resource "aws_iam_user_group_membership" "s3_group_membership" {
-  user = "${aws_iam_user.storage_user.name}"
+  user = aws_iam_user.storage_user.name
 
   groups = [
     "${aws_iam_group.s3_admin_group.name}",
@@ -17,7 +17,7 @@ resource "aws_iam_group" "query_instances_group" {
 
 resource "aws_iam_group_policy" "query_instances_group_policy" {
   name  = "query_instances_group_policy_pipeid${lookup(var.common_tags, "pipelineid", "0")}"
-  group = "${aws_iam_group.query_instances_group.id}"
+  group = aws_iam_group.query_instances_group.id
 
   policy = <<EOF
 {
@@ -44,7 +44,7 @@ resource "aws_iam_group" "s3_admin_group" {
 
 resource "aws_iam_group_policy" "s3_admin_group_policy" {
   name  = "s3_admin_group_policy_pipeid${lookup(var.common_tags, "pipelineid", "0")}"
-  group = "${aws_iam_group.s3_admin_group.id}"
+  group = aws_iam_group.s3_admin_group.id
 
   policy = <<EOF
 {
@@ -69,17 +69,19 @@ resource "aws_iam_user" "storage_user" {
 }
 
 resource "aws_iam_access_key" "storage_user_access_key" {
-  user    = "${aws_iam_user.storage_user.name}"
-  pgp_key = "${var.pgp_public_key}"
+  user    = aws_iam_user.storage_user.name
+  pgp_key = var.pgp_public_key
   # pgp key: normally in format 'keybase:my_username'
   # See https://www.hiroom2.com/2016/08/14/ubuntu-16-04-create-gpg-key/ to create pgp key in ubuntu
   # Also execllent documentation on pgp / gpg as an alternative to keybase http://zanussi.combell.org/bash_gpg_encrypt_decrypt.html
 }
 
 output "storage_user_access_key_id" {
+  depends_on = [aws_iam_user_group_membership.s3_group_membership]
   value = "${aws_iam_access_key.storage_user_access_key.id}"
 }
 
 output "storage_user_secret" {
+  depends_on = [aws_iam_user_group_membership.s3_group_membership]
   value = "${aws_iam_access_key.storage_user_access_key.encrypted_secret}"
 }
