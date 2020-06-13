b476deca8f1c84a6c0e9c41d877e37e2f0803a2e
Andrew Graham <queglay@gmail.com>
2020-06-13 20:17:23 +0930

    verbose

--- scripts/single-test.sh
+++ scripts/single-test.sh
@@ -3,5 +3,5 @@
 echo "TF_VAR_firehawk_path: $TF_VAR_firehawk_path"
 echo "TF_VAR_secrets_path: $TF_VAR_secrets_path"
 
-ansible-playbook -i ansible/inventory/hosts ansible/init.yaml --extra-vars "variable_user=deployuser set_hostname=ansiblecontrol"
-ansible-playbook -i "$TF_VAR_inventory" ansible/init.yaml --extra-vars "variable_host=firehawkgateway variable_user=deployuser configure_gateway=true set_hostname=firehawkgateway"
+ansible-playbook -i ansible/inventory/hosts ansible/init.yaml -v --extra-vars "variable_user=deployuser set_hostname=ansiblecontrol"
+ansible-playbook -i "$TF_VAR_inventory" ansible/init.yaml -v --extra-vars "variable_host=firehawkgateway variable_user=deployuser configure_gateway=true set_hostname=firehawkgateway"
