6734a2f808387e416a35b8bb94b623fbe2a72cd8
Andrew Graham <queglay@gmail.com>
2020-06-14 15:22:59 +0930

    remove softnas mode low

--- ansible/ansible_collections/firehawkvfx/softnas/terraform/softnas/variables.tf
+++ ansible/ansible_collections/firehawkvfx/softnas/terraform/softnas/variables.tf
@@ -86,16 +86,6 @@ variable "softnas2_private_ip2" {
 
 variable "init_aws_local_workstation" {}
 
-#softnas provides no ability to query the ami you will need by region.  it must be added to the map manually.
-variable "instance_type" {
-  type = map(string)
-
-  default = {
-    low  = "m4.xlarge"
-    high = "m4.2xlarge"
-  }
-}
-
 variable "softnas_instance_type" {
   default = "m4.xlarge"
 }
