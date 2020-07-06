9e7bf714a39e30309572607b5b1d14756ef21f22
Andrew Graham <andrew@firehawkvfx.com>
2020-07-06 13:15:09 +0930

    Add missing extension .sh [ci skip]

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -113,4 +113,4 @@ function ctrl_c() {
 $SCRIPTDIR/configure.sh
 
 echo "Source vars for dev and ensuring they are encrypted..."
-source ./update_vars --dev --init
+source ./update_vars.sh --dev --init
