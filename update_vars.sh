5f947cac36cd575114471c9152b1597968db9cba
Andrew Graham <user@MacBookPro13.local>
2020-07-06 10:14:19 +0930

    comment for trap error on linux only

--- update_vars.sh
+++ update_vars.sh
@@ -21,7 +21,7 @@ printf "\n${RED}Warning: Currently virtual box requires a version lock after ins
 echo "6.1.10 had problems with centos 7 and gnome 2020/07/07. Ensure you use the version below, or update with caution:"
 echo "yum install VirtualBox-6.1-6.1.8_137981_el7-1.x86_64 versionlock; yum versionlock add VirtualBox-6.1-6.1.8_137981_el7-1.x86_64"
 
-# This block allows you to echo a line number for a failure.
+# This block allows you to echo a line number for a failure. Only works on linux, mnot macos bash.
 # err_report() {
 #     echo "${BASH_SOURCE[0]}: $1 script err_report: Error on line $2"
 # }
