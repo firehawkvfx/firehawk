c4b1bf9f9dedf235ac1484f033ca3737f6fa9077
Andrew Graham <andrew@firehawkvfx.com>
2020-07-20 21:51:53 +0930

    try *.id

--- modules/workstation_pcoip/main.tf
+++ modules/workstation_pcoip/main.tf
@@ -323,6 +323,7 @@ resource "null_resource" "dependency_deadlinedb" {
 # ... and update the filters appropriately
 # We dont use image id's directly because they dont work in multiple regions.
 data "aws_ami" "pcoip_ami" {
+  count = 1
   most_recent      = true
   owners = ["679593333241"] # the account id
   filter {
@@ -342,7 +343,7 @@ variable "teradici_pcoip_ami_option" { # Where multiple data aws_ami queries are
 locals {
   keys = ["pcoip_ami"] # Where multiple data aws_ami queries are available, this is the full list of options.
   empty_list = list("")
-  values = ["${element( concat(data.aws_ami.pcoip_ami.ids, local.empty_list ), 0 )}"] # the list of ami id's
+  values = ["${element( concat(data.aws_ami.pcoip_ami.*.id, local.empty_list ), 0 )}"] # the list of ami id's
   teradici_pcoip_consumption_map = zipmap( local.keys , local.values )
 }
 
@@ -351,6 +352,7 @@ locals { # select the found ami to use based on the map lookup
 }
 
 data "aws_ami" "prebuilt_teradici_pcoip_ami_list" { # search for a prebuilt tagged ami with the same base image.  if there is a match, it can be used instead, allowing us to skip provisioning.
+  count = 1
   owners = ["self"]
   filter {
     name   = "tag:base_ami"
@@ -363,8 +365,8 @@ data "aws_ami" "prebuilt_teradici_pcoip_ami_list" { # search for a prebuilt tagg
 }
 
 locals {
-  prebuilt_teradici_pcoip_ami_list = data.aws_ami.prebuilt_teradici_pcoip_ami_list.ids
-  first_element = element( data.aws_ami.prebuilt_teradici_pcoip_ami_list.*.ids, 0)
+  prebuilt_teradici_pcoip_ami_list = data.aws_ami.prebuilt_teradici_pcoip_ami_list.*.id
+  first_element = element( data.aws_ami.prebuilt_teradici_pcoip_ami_list.*.id, 0)
   mod_list = concat( local.prebuilt_teradici_pcoip_ami_list , list("") )
   aquired_ami      = "${element( local.mod_list , 0)}" # aquired ami will use the ami in the list if found, otherwise it will default to the original ami.
   use_prebuilt_teradici_pcoip_ami = var.allow_prebuilt_teradici_pcoip_ami && length(local.mod_list) > 1 ? true : false
