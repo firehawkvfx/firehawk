09a3acddd451aa6321d9e289807cf1bccf4a8550
Andrew Graham <andrew@firehawkvfx.com>
2020-07-22 19:49:05 +0930

    run single test

--- scripts/single-test.sh
+++ scripts/single-test.sh
@@ -3,4 +3,4 @@
 echo "TF_VAR_firehawk_path: $TF_VAR_firehawk_path"
 echo "TF_VAR_secrets_path: $TF_VAR_secrets_path"
 
-aws ec2 describe-instances --filter "Name=instance-state-name,Values=running" --filter "Name=tag:conflictkey,Values=$TF_VAR_conflictkey" --query "Reservations[*].Instances[*][Tags[?Key=='Name'].Value[],NetworkInterfaces[0].PrivateIpAddresses[0].PrivateIpAddress]" --output text
+ansible-playbook -i ../secrets/dev/inventory ansible/newuser_sshuser.yaml -vvvv --extra-vars 'variable_host=workstation1 user_inituser_name=user ansible_ssh_private_key_file=/secrets/keys/id_ssh_rsa_dev'
