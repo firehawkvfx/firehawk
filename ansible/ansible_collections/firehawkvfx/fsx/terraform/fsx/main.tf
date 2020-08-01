0e02718fdc7c03aaa71a025ba6510008b63f7106
Andrew Graham <queglay@gmail.com>
2020-08-01 15:26:33 +0930

    network_interface_ids output

--- ansible/ansible_collections/firehawkvfx/fsx/terraform/fsx/main.tf
+++ ansible/ansible_collections/firehawkvfx/fsx/terraform/fsx/main.tf
@@ -16,8 +16,19 @@ EOT
 resource "aws_fsx_lustre_file_system" "fsx_storage" {
   count = var.fsx_storage ? 1 : 0
   depends_on = [ null_resource.init_fsx ]
+  
   import_path      = "s3://prod.${var.bucket_extension}"
   storage_capacity = 1200
   subnet_ids       = var.subnet_ids
+  deployment_type  = "SCRATCH_2"
+
   tags = var.common_tags
+}
+
+output "id" {
+  value = aws_fsx_lustre_file_system.fsx_storage.id
+}
+
+output "network_interface_ids" {
+  value = aws_fsx_lustre_file_system.fsx_storage.network_interface_ids
 }
