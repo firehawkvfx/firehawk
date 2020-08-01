eff84fa54ab8cfe2bf400b14d1c1f552df998b33
Andrew Graham <queglay@gmail.com>
2020-08-01 20:00:33 +0930

    test better string formatting

--- ansible/ansible_collections/firehawkvfx/fsx/terraform/fsx/primary_interface.sh
+++ ansible/ansible_collections/firehawkvfx/fsx/terraform/fsx/primary_interface.sh
@@ -3,19 +3,16 @@
 # Exit if any of the intermediate steps fail
 set -e
 
-# Extract "foo" and "baz" arguments from the input into
-# FOO and BAZ shell variables.
+# Extract arguments from the input into
+# shell variables.
 # jq will ensure that the values are properly quoted
 # and escaped for consumption by the shell.
 eval "$(jq -r '@sh "id=\(.id)"')"
 
-# Placeholder for whatever data-fetching logic your script implements
-
 primary_interface=$(aws fsx describe-file-systems | jq ".FileSystems[] | select(.FileSystemId == \"$id\") | .NetworkInterfaceIds[0]")
-
-# FOOBAZ="$FOO $BAZ"
 
 # Safely produce a JSON object containing the result value.
 # jq will ensure that the value is properly quoted
 # and escaped to produce a valid JSON string.
-jq -n --arg primary_interface "$primary_interface" '{"primary_interface":$primary_interface}'
+
+jq -n --arg primary_interface "$primary_interface" "{\"primary_interface\":$primary_interface}"
