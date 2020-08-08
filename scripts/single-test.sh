e0ceb62f06bbd1ce391251319c598d6330d38bc1
Andrew Graham <queglay@gmail.com>
2020-08-08 10:08:46 +0930

    prep single test again

--- scripts/single-test.sh
+++ scripts/single-test.sh
@@ -3,8 +3,7 @@
 echo "TF_VAR_firehawk_path: $TF_VAR_firehawk_path"
 echo "TF_VAR_secrets_path: $TF_VAR_secrets_path"
 
-
-ansible-playbook -i ../secrets/dev/inventory ansible/newuser_sshuser.yaml -v --extra-vars "variable_host=firehawkgateway user_inituser_name=deployuser user_inituser_pw='' ansible_ssh_private_key_file=/secrets/keys/firehawkgateway_private_key"
+# ansible-playbook -i ../secrets/dev/inventory ansible/newuser_sshuser.yaml -v --extra-vars "variable_host=firehawkgateway user_inituser_name=deployuser user_inituser_pw='' ansible_ssh_private_key_file=/secrets/keys/firehawkgateway_private_key"
 ansible-playbook -i ../secrets/dev/inventory ansible/ssh-add-public-host.yaml -v --extra-vars 'public_ip=54.253.227.162 public_address=54.253.227.162 bastion_address=54.253.227.162 set_bastion=true'
 
 # ansible-playbook -i ../secrets/dev/inventory ansible/newuser_sshuser.yaml -vvvv --extra-vars 'variable_host=workstation1 user_inituser_name=user ansible_ssh_private_key_file=/secrets/keys/id_ssh_rsa_dev'
