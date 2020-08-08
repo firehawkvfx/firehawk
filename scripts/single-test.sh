4bb0f1d4cba7dc164c0d5bbfa7e455d5b7d5dff4
Andrew Graham <queglay@gmail.com>
2020-08-08 14:03:24 +0930

    exit test

--- scripts/single-test.sh
+++ scripts/single-test.sh
@@ -3,4 +3,4 @@
 echo "TF_VAR_firehawk_path: $TF_VAR_firehawk_path"
 echo "TF_VAR_secrets_path: $TF_VAR_secrets_path"
 
-ansible-playbook -i ../secrets/dev/inventory ansible/ansible_collections/firehawkvfx/fsx/fsx_volume_mounts.yaml -vvv --extra-vars fsx_ip=10.1.1.248 --skip-tags 'local_install local_install_onsite_mounts' --tags cloud_install
+ansible-playbook -i ../secrets/dev/inventory ansible/ansible_collections/firehawkvfx/fsx/fsx_volume_mounts.yaml -vvv --extra-vars fsx_ip=10.1.1.248 --skip-tags 'local_install local_install_onsite_mounts' --tags cloud_install; exit_test
