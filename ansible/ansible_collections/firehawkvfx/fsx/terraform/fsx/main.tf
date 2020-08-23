7497492408f5db2366da57eaaa12067083c3c508
Andrew Graham <andrew@firehawkvfx.com>
2020-08-23 10:16:15 +0930

    add comma

--- ansible/ansible_collections/firehawkvfx/fsx/terraform/fsx/main.tf
+++ ansible/ansible_collections/firehawkvfx/fsx/terraform/fsx/main.tf
@@ -308,7 +308,7 @@ resource "null_resource" "attach_local_mounts_after_start" {
   depends_on = [
     aws_fsx_lustre_file_system.fsx_storage,
     data.external.primary_interface_id,
-    data.aws_network_interface.fsx_primary_interface
+    data.aws_network_interface.fsx_primary_interface,
     aws_route53_record.fsx_record
   ]
   triggers = {
