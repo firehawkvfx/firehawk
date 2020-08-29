3a9619261db52336deb9ef620766c6094c3fce4a
Andrew Graham <andrew@firehawkvfx.com>
2020-08-29 17:29:00 +0930

    single test

--- scripts/single-test.sh
+++ scripts/single-test.sh
@@ -3,4 +3,4 @@
 echo "TF_VAR_firehawk_path: $TF_VAR_firehawk_path"
 echo "TF_VAR_secrets_path: $TF_VAR_secrets_path"
 
-ansible-playbook -i ../secrets/dev/inventory ansible/openvpn.yaml -vvvv --extra-vars 'vpn_address=52.63.218.41 private_ip=10.1.101.136 private_subnet1=10.1.1.0/24 public_subnet1=10.1.101.0/24 remote_subnet_cidr=192.168.92.0/24 client_network=172.17.232.0 client_netmask_bits=24'
+ansible-playbook -i ../secrets/dev/inventory ansible/aws_cli_ec2_install.yaml -vv --extra-vars 'variable_host=workstation1 variable_user=deployuser aws_cli_root=true ansible_ssh_private_key_file=/secrets/keys/id_ssh_rsa_dev'
