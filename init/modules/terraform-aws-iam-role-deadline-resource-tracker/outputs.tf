f117714b59e513f684c095450a9ce5fa412c4f63
Andrew Graham <andrew@firehawkvfx.com>
2021-04-25 14:08:11 +0930

    rename output

--- init/modules/terraform-aws-iam-role-deadline-resource-tracker/outputs.tf
+++ init/modules/terraform-aws-iam-role-deadline-resource-tracker/outputs.tf
@@ -1,6 +1,6 @@
-output "instance_role_arn" {
+output "iam_role_arn" {
   value = aws_iam_role.instance_role.arn
 }
-output "instance_role_name" {
+output "iam_role_name" {
   value = aws_iam_role.instance_role.name
 }
