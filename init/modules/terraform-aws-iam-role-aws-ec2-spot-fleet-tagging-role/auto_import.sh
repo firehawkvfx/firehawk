8c4c2caa6abc413cdff443e91b5dd6501a15ca49
Andrew Graham <andrew@firehawkvfx.com>
2021-05-01 21:05:42 +0930

    suppress stderr

--- init/modules/terraform-aws-iam-role-aws-ec2-spot-fleet-tagging-role/auto_import.sh
+++ init/modules/terraform-aws-iam-role-aws-ec2-spot-fleet-tagging-role/auto_import.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "Determining if resource already exists in state file"
-output=$(terragrunt state list | grep -m 1 'aws_iam_role.service_role') && exit_status=0 || exit_status=$?
+echo "Determining if resource already exists in state file...  Ignore any immediate errors below"
+output=$(terragrunt state list | grep -m 1 'aws_iam_role.service_role') 2> /dev/null && exit_status=0 || exit_status=$?
 
 if [[ ! exit_status -eq 0 ]]; then # if not, then attempt import
     output=$(terragrunt import aws_iam_role.service_role aws-ec2-spot-fleet-tagging-role) && exit_status=0 || exit_status=$?
@@ -9,8 +9,12 @@ if [[ ! exit_status -eq 0 ]]; then # if not, then attempt import
         echo 'The iam role will be created by terraform'
     else
         echo "$output"
+        echo
         echo "The resource was imported."
+        echo
     fi
 else
+    echo
     echo "Resource already exists, no auto-import required"
+    echo
 fi
