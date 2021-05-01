e6285accb21b671e7e24e7d862b1d0b8013e2f4d
Andrew Graham <andrew@firehawkvfx.com>
2021-05-01 21:20:24 +0930

    update other redirect

--- init/modules/terraform-aws-iam-role-aws-ec2-spot-fleet-tagging-role/auto_import.sh
+++ init/modules/terraform-aws-iam-role-aws-ec2-spot-fleet-tagging-role/auto_import.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "Determining if resource already exists in state file...  Ignore any immediate errors below"
-output=$(terragrunt state list | grep -m 1 'aws_iam_role.service_role') 2> /dev/null && exit_status=0 || exit_status=$?
+echo "Determining if resource already exists in state file..."
+output=$((terragrunt state list | grep -m 1 'aws_iam_role.service_role') 2>&1) && exit_status=0 || exit_status=$?
 
 if [[ ! exit_status -eq 0 ]]; then # if not, then attempt import
     output=$((terragrunt import aws_iam_role.service_role aws-ec2-spot-fleet-tagging-role) 2>&1) && exit_status=0 || exit_status=$?
