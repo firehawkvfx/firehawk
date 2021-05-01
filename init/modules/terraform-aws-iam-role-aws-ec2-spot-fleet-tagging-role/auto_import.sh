bfa4db33f89156804928a9a63cfca52d9965358b
Andrew Graham <andrew@firehawkvfx.com>
2021-05-01 21:08:27 +0930

    redirect stderr

--- init/modules/terraform-aws-iam-role-aws-ec2-spot-fleet-tagging-role/auto_import.sh
+++ init/modules/terraform-aws-iam-role-aws-ec2-spot-fleet-tagging-role/auto_import.sh
@@ -4,9 +4,11 @@ echo "Determining if resource already exists in state file...  Ignore any immedi
 output=$(terragrunt state list | grep -m 1 'aws_iam_role.service_role') 2> /dev/null && exit_status=0 || exit_status=$?
 
 if [[ ! exit_status -eq 0 ]]; then # if not, then attempt import
-    output=$(terragrunt import aws_iam_role.service_role aws-ec2-spot-fleet-tagging-role) && exit_status=0 || exit_status=$?
+    output=$(terragrunt import aws_iam_role.service_role aws-ec2-spot-fleet-tagging-role) 2> /dev/null && exit_status=0 || exit_status=$?
     if [[ ! exit_status -eq 0 ]]; then # if import failed, assume we will be able to create it with terraform
+        echo
         echo 'The iam role will be created by terraform'
+        echo
     else
         echo "$output"
         echo
