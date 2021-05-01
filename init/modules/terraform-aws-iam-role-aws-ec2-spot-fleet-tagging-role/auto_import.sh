167629390ff72a446ca9ae7f17569f59dfc36c5b
Andrew Graham <andrew@firehawkvfx.com>
2021-05-01 20:54:58 +0930

    update auto import method

--- init/modules/terraform-aws-iam-role-aws-ec2-spot-fleet-tagging-role/auto_import.sh
+++ init/modules/terraform-aws-iam-role-aws-ec2-spot-fleet-tagging-role/auto_import.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-terragrunt state list | grep -m 1 'aws_iam_role.service_role' || terragrunt import aws_iam_role.service_role aws-ec2-spot-fleet-tagging-role || echo 'The iam role will be created by terraform'
+echo "Determining if resource already exists in state file"
+output=$(terragrunt state list | grep -m 1 'aws_iam_role.service_role') && exit_status=0 || exit_status=$?
+
+if [[ ! exit_status -eq 0 ]]; then # if not, then attempt import
+    output=$(terragrunt import aws_iam_role.service_role aws-ec2-spot-fleet-tagging-role) && exit_status=0 || exit_status=$?
+    if [[ ! exit_status -eq 0 ]]; then # if import failed, assume we will be able to create it with terraform
+        echo 'The iam role will be created by terraform'
+    else
+        echo "$output"
+        echo "The resource was imported."
+    fi
+else
+    echo "Resource already exists, no auto-import required"
+fi
