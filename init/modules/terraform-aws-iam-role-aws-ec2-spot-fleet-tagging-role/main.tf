3afe3e327a53ec50acc4acb91421c3dd3bd584d5
Andrew Graham <andrew@firehawkvfx.com>
2021-05-01 18:15:03 +0930

    update incorrect arn

--- init/modules/terraform-aws-iam-role-aws-ec2-spot-fleet-tagging-role/main.tf
+++ init/modules/terraform-aws-iam-role-aws-ec2-spot-fleet-tagging-role/main.tf
@@ -4,7 +4,7 @@ resource "aws_iam_role" "instance_role" {
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
   tags = merge( var.common_tags, map( "role", "deadline") )
   managed_policy_arns = [
-    "arn:aws:iam::aws:policy/AmazonEC2SpotFleetTaggingRole"
+    "arn:aws:iam::aws:policy/service-role/AmazonEC2SpotFleetTaggingRole"
   ]
 }
 data "aws_iam_policy_document" "assume_role" { # Determines the services able to assume the role.
