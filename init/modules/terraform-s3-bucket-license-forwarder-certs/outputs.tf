b3f056f568a3ed88851abc07a30e577b2e330d65
Andrew Graham <queglay@gmail.com>
2021-05-22 23:21:19 +0930

    cleanup depends

--- init/modules/terraform-s3-bucket-license-forwarder-certs/outputs.tf
+++ init/modules/terraform-s3-bucket-license-forwarder-certs/outputs.tf
@@ -4,7 +4,7 @@ output "s3_bucket_arn" {
 }
 
 output "s3_bucket_name" {
-  depends_on = [aws_s3_bucket.license_forwarder_cert_bucket.arn]
+  depends_on = [aws_s3_bucket.license_forwarder_cert_bucket]
   value       = local.bucket_name
   # value = aws_s3_bucket.license_forwarder_cert_bucket.bucket_domain_name
   description = "The name of the license forwarder certs bucket"
