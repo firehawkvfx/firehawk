4680c68d2016358376e00085658b1dab6d343a33
Andrew Graham <queglay@gmail.com>
2021-05-22 23:20:34 +0930

    update instructions

--- init/modules/terraform-s3-bucket-license-forwarder-certs/outputs.tf
+++ init/modules/terraform-s3-bucket-license-forwarder-certs/outputs.tf
@@ -4,8 +4,8 @@ output "s3_bucket_arn" {
 }
 
 output "s3_bucket_name" {
-  # depends_on = [aws_s3_bucket.license_forwarder_cert_bucket.arn]
-  # value       = local.bucket_name
-  value = aws_s3_bucket.license_forwarder_cert_bucket.bucket_domain_name
+  depends_on = [aws_s3_bucket.license_forwarder_cert_bucket.arn]
+  value       = local.bucket_name
+  # value = aws_s3_bucket.license_forwarder_cert_bucket.bucket_domain_name
   description = "The name of the license forwarder certs bucket"
 }
