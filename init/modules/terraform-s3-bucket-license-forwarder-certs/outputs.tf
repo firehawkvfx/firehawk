a01527476c193e6fafc1b52c3d191686093eaf4d
Andrew Graham <queglay@gmail.com>
2021-05-22 21:39:54 +0930

    update bad output

--- init/modules/terraform-s3-bucket-license-forwarder-certs/outputs.tf
+++ init/modules/terraform-s3-bucket-license-forwarder-certs/outputs.tf
@@ -1,4 +1,4 @@
 output "s3_bucket_arn" {
-  value       = aws_s3_bucket.vault_backend.arn
-  description = "The ARN of the S3 bucket"
+  value       = aws_s3_bucket.license_forwarder_cert_bucket.arn
+  description = "The ARN of the license forwarder certs bucket"
 }
