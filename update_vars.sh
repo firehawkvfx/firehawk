8e99735a27e6b8cf9fdd4bc5382b60f809dfaa55
Andrew Graham <queglay@gmail.com>
2021-05-29 12:55:12 +0930

    add ability to skip find ami

--- update_vars.sh
+++ update_vars.sh
@@ -88,6 +88,7 @@ SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" # 
 
 function export_vars {
   local -r latest_ami="$1"
+  local -r skip_find_amis="$2"
   # Region is required for AWS CLI
   export AWS_DEFAULT_REGION=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/\(.*\)[a-z]/\1/')
   # Get the resourcetier from the instance tag.
@@ -144,35 +145,37 @@ function export_vars {
   # AMI query by commit - Vault and Consul Servers
   export TF_VAR_ami_commit_hash="$(cd $TF_VAR_firehawk_path/../packer-firehawk-amis/modules/firehawk-ami; git rev-parse HEAD)" 
 
-  # AMI query by commit - Vault and Consul Server
-  ami_role="firehawk_ubuntu18_vault_consul_server_ami"
-  export TF_VAR_vault_consul_ami_id=$(retrieve_ami $latest_ami $ami_role $TF_VAR_ami_commit_hash)
-  warn_if_invalid "$ami_role" "$TF_VAR_vault_consul_ami_id" "TF_VAR_vault_consul_ami_id"
-  # AMI query by commit - Vault and Consul Client
-  ami_role="firehawk_centos7_ami"
-  export TF_VAR_vault_client_ami_id=$(retrieve_ami $latest_ami $ami_role $TF_VAR_ami_commit_hash)
-  warn_if_invalid "$ami_role" "$TF_VAR_vault_client_ami_id" "TF_VAR_vault_client_ami_id"
-  # AMI query by commit - Bastion Host
-  ami_role="firehawk_centos7_ami"
-  export TF_VAR_bastion_ami_id=$(retrieve_ami $latest_ami $ami_role $TF_VAR_ami_commit_hash)
-  warn_if_invalid "$ami_role" "$TF_VAR_bastion_ami_id" "TF_VAR_bastion_ami_id"
-  # AMI query by commit - Open VPN Server
-  ami_role="firehawk_openvpn_server_ami"
-  export TF_VAR_openvpn_server_ami=$(retrieve_ami $latest_ami $ami_role $TF_VAR_ami_commit_hash)
-  warn_if_invalid "$ami_role" "$TF_VAR_openvpn_server_ami" "TF_VAR_openvpn_server_ami"
-  # AMI query by commit - Deadline DB
-  ami_role="firehawk_deadlinedb_ami"
-  export TF_VAR_deadline_db_ami_id=$(retrieve_ami $latest_ami $ami_role $TF_VAR_ami_commit_hash)
-  warn_if_invalid "$ami_role" "$TF_VAR_deadline_db_ami_id" "TF_VAR_deadline_db_ami_id"
-  # AMI query by commit - Render node
-  ami_role="firehawk_centos7_rendernode_ami"
-  export TF_VAR_node_centos7_houdini_ami_id=$(retrieve_ami $latest_ami $ami_role $TF_VAR_ami_commit_hash)
-  warn_if_invalid "$ami_role" "$TF_VAR_node_centos7_houdini_ami_id" "TF_VAR_node_centos7_houdini_ami_id"
-  # AMI query by commit - Workstation
-  ami_role="firehawk_amazonlinux2_nicedcv_ami"
-  export TF_VAR_workstation_amazonlinux2_nicedcv_ami_id=$(retrieve_ami $latest_ami $ami_role $TF_VAR_ami_commit_hash)
-  warn_if_invalid "$ami_role" "$TF_VAR_workstation_amazonlinux2_nicedcv_ami_id" "TF_VAR_workstation_amazonlinux2_nicedcv_ami_id"
-
+  if [[ "$skip_find_amis" == "false" ]]; then
+    # AMI query by commit - Vault and Consul Server
+    ami_role="firehawk_ubuntu18_vault_consul_server_ami"
+    export TF_VAR_vault_consul_ami_id=$(retrieve_ami $latest_ami $ami_role $TF_VAR_ami_commit_hash)
+    warn_if_invalid "$ami_role" "$TF_VAR_vault_consul_ami_id" "TF_VAR_vault_consul_ami_id"
+    # AMI query by commit - Vault and Consul Client
+    ami_role="firehawk_centos7_ami"
+    export TF_VAR_vault_client_ami_id=$(retrieve_ami $latest_ami $ami_role $TF_VAR_ami_commit_hash)
+    warn_if_invalid "$ami_role" "$TF_VAR_vault_client_ami_id" "TF_VAR_vault_client_ami_id"
+    # AMI query by commit - Bastion Host
+    ami_role="firehawk_centos7_ami"
+    export TF_VAR_bastion_ami_id=$(retrieve_ami $latest_ami $ami_role $TF_VAR_ami_commit_hash)
+    warn_if_invalid "$ami_role" "$TF_VAR_bastion_ami_id" "TF_VAR_bastion_ami_id"
+    # AMI query by commit - Open VPN Server
+    ami_role="firehawk_openvpn_server_ami"
+    export TF_VAR_openvpn_server_ami=$(retrieve_ami $latest_ami $ami_role $TF_VAR_ami_commit_hash)
+    warn_if_invalid "$ami_role" "$TF_VAR_openvpn_server_ami" "TF_VAR_openvpn_server_ami"
+    # AMI query by commit - Deadline DB
+    ami_role="firehawk_deadlinedb_ami"
+    export TF_VAR_deadline_db_ami_id=$(retrieve_ami $latest_ami $ami_role $TF_VAR_ami_commit_hash)
+    warn_if_invalid "$ami_role" "$TF_VAR_deadline_db_ami_id" "TF_VAR_deadline_db_ami_id"
+    # AMI query by commit - Render node
+    ami_role="firehawk_centos7_rendernode_ami"
+    export TF_VAR_node_centos7_houdini_ami_id=$(retrieve_ami $latest_ami $ami_role $TF_VAR_ami_commit_hash)
+    warn_if_invalid "$ami_role" "$TF_VAR_node_centos7_houdini_ami_id" "TF_VAR_node_centos7_houdini_ami_id"
+    # AMI query by commit - Workstation
+    ami_role="firehawk_amazonlinux2_nicedcv_ami"
+    export TF_VAR_workstation_amazonlinux2_nicedcv_ami_id=$(retrieve_ami $latest_ami $ami_role $TF_VAR_ami_commit_hash)
+    warn_if_invalid "$ami_role" "$TF_VAR_workstation_amazonlinux2_nicedcv_ami_id" "TF_VAR_workstation_amazonlinux2_nicedcv_ami_id"
+  fi
+  
   # Terraform Vars
   export TF_VAR_general_use_ssh_key="$HOME/.ssh/id_rsa" # For debugging deployment of most resources- not for production use.
   export TF_VAR_aws_private_key_path="$TF_VAR_general_use_ssh_key"
@@ -261,12 +264,16 @@ function options { # Not all defaults are available as args, however the script 
   local latest_ami="$DEFAULT_LATEST_AMI"
   local run="true"
   local sub_script="true"
+  local skip_find_amis="false"
 
   while [[ $# > 0 ]]; do
     local key="$1"
     case "$key" in
       --latest-ami)
         latest_ami="true"
+        ;;
+      --skip-find-amis)
+        skip_find_amis="true"
         ;;
       --help)
         print_usage
@@ -284,7 +291,7 @@ function options { # Not all defaults are available as args, however the script 
     shift
   done
   if [[ "$run" == "true" ]]; then
-    export_vars "$latest_ami"
+    export_vars "$latest_ami" "$skip_find_amis"
   fi
 }
 
