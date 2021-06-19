de538e218b6567c05e40a70d475ae2c9e07a2001
Andrew Graham <queglay@gmail.com>
2021-06-19 10:35:49 +0930

    update name

--- init/modules/terraform-aws-sqs-vpn/main.tf
+++ init/modules/terraform-aws-sqs-vpn/main.tf
@@ -2,7 +2,7 @@
 
 resource "aws_sqs_queue" "remote_in_vpn" { # the queue that your remote vpn host will poll for certificates
   kms_master_key_id                 = "alias/aws/sqs"
-  name      = "remote_in_vpn_${lookup(var.common_tags, "resourcetier", "0")}${lookup(var.common_tags, "pipelineid", "0")}"
+  name      = "remote-in-vpn-${lookup(var.common_tags, "resourcetier", "0")}${lookup(var.common_tags, "pipelineid", "0")}"
   kms_data_key_reuse_period_seconds = 300
   fifo_queue                        = true
   content_based_deduplication       = true
