fcb3ce86116270b6dbcc2f2e8cd8ea9e71d69771
Andrew Graham <queglay@gmail.com>
2021-06-19 16:52:56 +0930

    typo for var name

--- vault-init/firehawk-main/modules/vault-aws-creds-deadline-cert/data/outputs.tf
+++ vault-init/firehawk-main/modules/vault-aws-creds-deadline-cert/data/outputs.tf
@@ -5,5 +5,5 @@ output "remote_in_cert_arn" {
   value = data.terraform_remote_state.terraform_aws_sqs_ssh.outputs.remote_in_cert_arn
 }
 output "remote_in_deadline_cert_arn" {
-  value = data.terraform_remote_state.terraform_aws_sqs_deadline_cert.outputs.remote_in_vpn_arn
+  value = data.terraform_remote_state.terraform_aws_sqs_deadline_cert.outputs.remote_in_deadline_cert_arn
 }
