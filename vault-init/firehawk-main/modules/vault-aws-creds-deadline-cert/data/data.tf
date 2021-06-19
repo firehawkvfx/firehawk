6112054ffaf25394b1e9cdc238b8094019a3ae80
Andrew Graham <queglay@gmail.com>
2021-06-19 18:47:59 +0930

    update path

--- vault-init/firehawk-main/modules/vault-aws-creds-deadline-cert/data/data.tf
+++ vault-init/firehawk-main/modules/vault-aws-creds-deadline-cert/data/data.tf
@@ -1,5 +1,5 @@
 data "aws_region" "current" {}
-data "terraform_remote_state" "terraform_aws_sqs_ssh" { # read the arn with data.terraform_remote_state.packer_profile.outputs.instance_role_arn, or read the profile name with data.terraform_remote_state.packer_profile.outputs.instance_profile_name
+data "terraform_remote_state" "terraform_aws_sqs_ssh" {
   backend = "s3"
   config = {
     bucket = "state.terraform.${var.bucket_extension_vault}"
@@ -7,7 +7,7 @@ data "terraform_remote_state" "terraform_aws_sqs_ssh" { # read the arn with data
     region = data.aws_region.current.name
   }
 }
-data "terraform_remote_state" "terraform_aws_sqs_deadline_cert" { # read the arn with data.terraform_remote_state.packer_profile.outputs.instance_role_arn, or read the profile name with data.terraform_remote_state.packer_profile.outputs.instance_profile_name
+data "terraform_remote_state" "terraform_aws_sqs_deadline_cert" {
   backend = "s3"
   config = {
     bucket = "state.terraform.${var.bucket_extension_vault}"
@@ -15,11 +15,11 @@ data "terraform_remote_state" "terraform_aws_sqs_deadline_cert" { # read the arn
     region = data.aws_region.current.name
   }
 }
-data "terraform_remote_state" "terraform_aws_secret_backend" { # read the arn with data.terraform_remote_state.packer_profile.outputs.instance_role_arn, or read the profile name with data.terraform_remote_state.packer_profile.outputs.instance_profile_name
+data "terraform_remote_state" "terraform_aws_secret_backend" {
   backend = "s3"
   config = {
     bucket = "state.terraform.${var.bucket_extension_vault}"
-    key    = "vault-init/firehawk-main/modules/vault-aws-secret-backend/module/terraform.tfstate"
+    key    = "firehawk-main/modules/vault-aws-secret-backend/module/terraform.tfstate"
     region = data.aws_region.current.name
   }
 }
