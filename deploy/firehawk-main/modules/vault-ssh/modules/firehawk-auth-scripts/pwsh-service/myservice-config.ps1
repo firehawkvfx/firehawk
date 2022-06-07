d466043c6d084d36ccf33bb7ddc2c1a1cf46ca68
Andrew Graham <queglay@gmail.com>
2022-06-07 21:21:05 +0930

    Remove firehawk-auth-scripts

--- deploy/firehawk-main/modules/vault-ssh/modules/firehawk-auth-scripts/pwsh-service/myservice-config.ps1
+++
@@ -1,7 +0,0 @@
-#Requires -Version 7.0
-
-$resourcetier = 'REPLACE_WITH_RESOURCETIER'
-$deadline_user_name = 'REPLACE_WITH_DEADLINE_USER_NAME' 
-$aws_region = 'REPLACE_WITH_AWS_REGION' 
-$aws_access_key ='REPLACE_WITH_AWS_ACCESS_KEY' 
-$aws_secret_key ='REPLACE_WITH_AWS_SECRET_KEY'
