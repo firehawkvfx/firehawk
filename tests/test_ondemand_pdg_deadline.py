37664fa5839a5be280ec046ef275b2208c9051cd
Andrew Graham <queglay@gmail.com>
2022-06-13 10:55:06 +0930

    Test byo

--- tests/test_ondemand_pdg_deadline.py
+++ tests/test_ondemand_pdg_deadline.py
@@ -1,7 +1,8 @@
 # This file is used in the .sh script to test cooking with preflight.
 
 import hou
-node=hou.node('/obj/topnet1/ropfetch_ondemand_ubl')
+# node=hou.node('/obj/topnet1/ropfetch_ondemand_ubl') # This tests engine with a UBL license
+node=hou.node('/obj/topnet1/ropfetch_ondemand_byo_license')
 
 if hou.isUIAvailable():
     from nodegraphtopui import cookNode
