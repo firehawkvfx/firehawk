cc72c83817f404e77c724f7216671ed283edb3da
Andrew Graham <queglay@gmail.com>
2022-10-24 08:48:59 +1100

    Update to H19.5 and deadline 10.1.23.6

--- update_vars.sh
+++ update_vars.sh
@@ -174,7 +174,7 @@ function export_vars {
   export TF_VAR_firehawk_path="$SCRIPTDIR/deploy/firehawk-main"
 
   # Packer Vars
-  export TF_VAR_deadline_version="10.1.21.4"
+  export TF_VAR_deadline_version="10.1.23.6"
   export PKR_VAR_deadline_version="$TF_VAR_deadline_version"
 
   export PACKER_LOG=1
